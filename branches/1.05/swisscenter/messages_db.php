342568cf5326ebcc9dec3eae9429b74aafd195aa
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2004-11-26 14:22:50 +0000

    Added $page parameter to the function get_new_and_unread_messages as it was being referenced out of scope

--- branches/1.05/swisscenter/messages_db.php
+++ branches/1.05/swisscenter/messages_db.php
@@ -5,29 +5,44 @@
 
   require_once("base/mysql.php");
 
-  define('MESSAGE_STATUS_NEW', 0);
-  define('MESSAGE_STATUS_READ', 1);
+  define('MESSAGE_STATUS_NEW'    , 0);
+  define('MESSAGE_STATUS_READ'   , 1);
   define('MESSAGE_STATUS_DELETED', 2);
   
   $message_status_string = array(
-      MESSAGE_STATUS_NEW => "New",
-      MESSAGE_STATUS_READ => "Read",
+      MESSAGE_STATUS_NEW     => "New",
+      MESSAGE_STATUS_READ    => "Read",
       MESSAGE_STATUS_DELETED => "Deleted",
   );
   
+  function delete_message($delete_id)
+  {
+    // Delete the message from the database
+    $success = db_sqlcommand("UPDATE messages 
+                                 SET status=".MESSAGE_STATUS_DELETED." 
+                               WHERE message_id=".$delete_id);
+  }
+  
   function count_messages_with_status($status)
   {
-    return db_value("select count(*) from messages where status=".$status);
+    return db_value("SELECT count(*) 
+                       FROM messages
+                      WHERE status=".$status);
   }
 
   function count_new_and_unread_messages()
   {
-    return db_value("select count(*) from messages where status<=".MESSAGE_STATUS_READ);
+    return db_value("SELECT count(*)
+                       FROM messages 
+                      WHERE status<=".MESSAGE_STATUS_READ);
   }
   
-  function get_new_and_unread_messages()
+  function get_new_and_unread_messages( $page = 0 )
   {
-    return db_toarray("select * from messages where status <= ".MESSAGE_STATUS_READ." order by added desc limit "
-              .(($page*MAX_PER_PAGE)).",".MAX_PER_PAGE);
+    return db_toarray("SELECT * 
+                         FROM messages
+                        WHERE status IN (".MESSAGE_STATUS_NEW.",".MESSAGE_STATUS_READ.") 
+                     ORDER BY added desc 
+                        LIMIT ".($page*MAX_PER_PAGE).",".MAX_PER_PAGE);
   }
 ?>
