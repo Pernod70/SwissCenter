2adfe8abbb1451a01ff8bad2f2f723717d52a4a2
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2004-11-28 23:09:30 +0000

    Fixed streaming problems on winamp/showcenter (media player was fine though!).
    - "Content-Range" should be specified as "Start-End/Size" - where the maximum value of "End" can be "Size"-1.
    
    - "Content-Length" is the number of bytes returned, not the total size of the file.
    
    - "HTTP/1.1 206 Partial content" must be returned for partial content (fancy that!) otherwise some players exhibit strange behavior when seeking, such as only being able to seek to 50%.

--- branches/1.05/swisscenter/stream.php
+++ branches/1.05/swisscenter/stream.php
@@ -6,8 +6,7 @@
   require_once("base/settings.php");
   require_once("base/file.php");
 
-
-  $startArray = sscanf( $_SERVER[ "HTTP_RANGE" ], "bytes=%d-" );
+  $startArray = sscanf( $_SERVER["HTTP_RANGE"], "bytes=%d-" );
   $start      = (empty($startArray[0]) ? 0 : $startArray[0]);
   $file       = stripcslashes($_REQUEST["file"]);
   $size       = filesize($file);
@@ -16,16 +15,20 @@
   // to "resume playing" whatever the user last chose, in conjunction with a temp playlist file).
   
   send_to_log("File  : ".$file); 
-  send_to_log("Range : ".$start."-".$size."/".$size); 
+  send_to_log("Range : ".$start."-".($size-1)."/".$size); 
 
   // Output page header
+
+  if ($start > 0)
+    header( "HTTP/1.1 206 Partial content" );
+  
   header("Pragma: ");
   header("Cache-Control: ");
   header("Content-type: application/octet-stream");
   header("Content-Disposition: inline; filename=".basename($file)); 
   header("Accept-Ranges: bytes"); 
-  header("Content-Length: ".(string)$size); 
-  header("Content-Range: bytes ".$start."-".$size."/".$size); 
+  header("Content-Range: bytes ".$start."-".($size-1)."/".$size); 
+  header("Content-Length: ".(string)($size-$start)); 
 
   // Stream the specified file 
   $fp = fopen($file, 'rb');
