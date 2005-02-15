2f1e91094694bd1a7a5554a6c1ce1148563b0738
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2005-02-15 17:21:19 +0000

    Added menu items for: - new messages - change screen type (NTSC/PAL)

--- trunk/swisscenter/config.php
+++ trunk/swisscenter/config.php
@@ -4,14 +4,16 @@
  *************************************************************************************************/
 
   require_once("base/page.php");
-
+  require_once("messages_db.php");
+  
   page_header( "Preferences and Setup" );
 
-  echo '<p>Please select an option from the list:';
+  echo '<p align="center">Please select an option from the list:';
 
   $menu = new menu();
   $menu->add_item("Change User Interface Style",'style.php',true);
   $menu->add_item("Search for new media",'do_refresh.php');
+  $menu->add_item("Change Screen Format",'config_screen.php',true);
 
   // Does the User have internet connectivity?
   if ($_SESSION["internet"])
@@ -19,15 +21,14 @@
     $menu->add_item("Update SwissCenter",'run_update.php');  
   }
 
-/* TODO
   // Are there any new messages to display to the user?
-  $num = db_value("select count(*) from messages where deleted is null");
-  if ($num >0)
+  $num_read = count_messages_with_status(MESSAGE_STATUS_READ);
+  $num_new = count_messages_with_status(MESSAGE_STATUS_NEW);
+  if(($num_read + $num_new) > 0)
   {
-    $menu->add_item("View Messages (".$num.")",'messages.php',true);
+    $menu->add_item("View Messages (".$num_new." new, ".$num_read." read)",'messages.php?return='.current_url(),true);
   }
-*/
-   
+
   $menu->display();
 
   page_footer( 'index.php' );
