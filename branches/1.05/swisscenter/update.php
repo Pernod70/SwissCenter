b8acab7279889cbcd3b0c3e26b41f6182a6ff0fa
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2005-02-17 20:47:18 +0000

    Rolled back to know working version (haven't tested Robs new changes yet, and I want to get a release done).

--- branches/1.05/swisscenter/update.php
+++ branches/1.05/swisscenter/update.php
@@ -30,58 +30,6 @@
    set_sys_pref('last_update',$last_update);
    $_SESSION["update"]["available"] = false;
  }
- 
- function run_sql_files($sql_files)
- {
-   $errors = 0;
-   
-   if(count($sql_files) > 0)
-   {
-     $last_sql_file_processed = $_SESSION["opts"]["database_vn"];
-
-     // Key sort to ensure they are in the correct order
-     ksort($sql_files, SORT_NUMERIC);
-
-     foreach($sql_files as $sql_file_num => $sql_file)
-     {
-       send_to_log("Processing SQL file ".$sql_file_num);
-       foreach ( split(";",implode(" ",file($sql_file))) as $sql)
-       {
-         if ( strlen(trim($sql)) > 0 ) 
-         {
-           if (db_sqlcommand($sql))
-             send_to_log("SQL command executed : ".$sql);
-           else 
-           {
-             send_to_log("SQL command failed : ".$sql);
-             $errors++;
-             // Exit the sql update procedure at this point as further files may depend on these changes
-             break 2;
-           }
-         }
-       }
-
-       $last_sql_file_processed = $sql_file_num;
-     }
-
-     // Update the db with the last applied file
-     db_sqlcommand("UPDATE system_prefs SET value='".$last_sql_file_processed."' WHERE name='DATABASE_VN'");
-   }
-   
-   return $errors;
- }
- 
- 
- function find_fileinfo($file_info_array, $filename)
- {
-   foreach($file_info_array as $file_info)
-   {
-     if($file_info["filename"] == $filename)
-       return &$file_info;
-   }
-   
-   return 0;
- }
    
 //*************************************************************************************************
 // Main Code
@@ -91,13 +39,9 @@
   $local   = array();
   $update  = array();
   $actions = array();
-  $sql_files = array();
-  $upd_script_name = "update.php";
   $upd_loc = 'http://update.swisscenter.co.uk/release/';
   $errors  = 0;
   $updated = false;
-  $update_updatefile_only = false;
-  $current_sql_version = $_SESSION["opts"]["database_vn"];
   
   // Get file checksums from the online update file  
   $file_contents = file_get_contents($upd_loc.'filelist.txt');
@@ -122,43 +66,13 @@
       mkdir('updates');
     }
     
-    // Check to see if there is an update to the update script (update.php)
-    $remote_update_file = find_fileinfo($update["files"], $upd_script_name);
-    $local_update_file = find_fileinfo($local, $upd_script_name);
-    if(($remote_update_file != 0) && ($local_update_file != 0) 
-       && ($remote_update_file["checksum"] != $local_update_file["checksum"]))
-    {
-      // There is a new update file, ensure that it is the only thing downloaded
-      send_to_log($upd_script_name." has changed, updating that file only and restarting update");
-      $update = array("files" => array("filename"=>$upd_script_name, "checksum"=>$remote_update_file["checksum"]));
-      $update_updatefile_only = true;
-    }
-    
-    
     // Download the required files    
     foreach ($update["files"] as $test)
     {   
       $filename = md5( $test["filename"].$test["checksum"]).'.bin';
       $tmp_file = 'updates/'.md5( $test["filename"].$test["checksum"]).'.update';
-      $skip_file = false;
   
-      if(file_ext($test["filename"]) == 'sql')
-      {
-        // Its an SQL file, check the file number and ignore files that we've already applied
-        // The file number is expected to be between the last _ and the . of the extension
-        $file_num = substr(strrchr($test["filename"], "_"), 1);
-        $file_num = substr($file_num,0,strpos($file_num,"."));
-
-        // If we couldn't get the file number or the file is <= the current version then skip        
-        if(($file_num === false) || ($file_num <= $current_sql_version) || !is_numeric($file_num))
-          $skip_file = true;
-        else
-          $sql_files[$file_num] = $tmp_file;
-      }
-  
-      // Don't download files that we've alread got or that we've been told to skip
-      // I.e. SQL files that are older than the current DB vn or invalid
-      if (!in_array($test,$local) && !$skip_file)
+      if (!in_array($test,$local) )
       {
         if (file_exists($tmp_file))
         {
@@ -233,52 +147,35 @@
     {
       foreach($actions as $a)
       {
-        // If the file is a sql file then skip it for now, we'll process them after the renames
-        if(!in_array($a["old"], $sql_files))
+        unlink($a["new"]);
+        rename($a["old"],$a["new"]);
+        send_to_log("'".$a["new"]."' updated");
+
+        // If the file that has been updated is an SQL file, then apply it to the database
+        if (strtolower(file_ext($a["new"])) == 'sql')
         {
-          unlink($a["new"]);
-          rename($a["old"],$a["new"]);
-          send_to_log("'".$a["new"]."' updated");
-        }
-        else
-          send_to_log("'".$a["new"]."' rename skipped, is a SQL file");
+          foreach ( split(";",implode(" ",file($a["new"]))) as $sql)
+            if ( strlen(trim($sql)) > 0 ) 
+            {
+              if (db_sqlcommand($sql))
+                send_to_log("SQL command executed : ".$sql);
+              else 
+                send_to_log("SQL command failed : ".$sql);
+            }
+        } 
       }
-      
-      // Run the SQL
-      $errors += run_sql_files($sql_files);
 
       $updated = true;
       force_rmdir('updates');
 
       // Update complete, so save file list for comparison to new updates
       $out = fopen("filelist.txt", "w");
-      
-      // If this was an update of the updatefile only then, update the local filelist to
-      // have the new update file in it
-      if($update_updatefile_only)
-      {
-        $local_update_file["checksum"] = $remote_update_file["checksum"];
-        fwrite($out, serialize($local));
-      }
-      else
-        fwrite($out, serialize($update["files"]));
-        
+      fwrite($out, serialize($update["files"]));
       fclose($out);   
       set_last_update();
       send_to_log("Update complete");
 
-      
-      if($update_updatefile_only)
-      {
-        // Re-run the update with the new script
-        send_to_log("Update script changed, restarting update with new script");
-        header("Location: /update.php");
-      }
-      else
-      {
-        // The update is complete
-        header("Location: /update_outcome.php?status=UPDATED");
-      }
+      header("Location: /update_outcome.php?status=UPDATED");
    }
    else 
    {
