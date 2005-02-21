ed15048337435cdb3fee0ea39c90348cc2a9323d
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2005-02-21 14:03:36 +0000

    Changed the way that files are selected (using SQL instead of just passing the filename).

--- branches/1.05/swisscenter/music_browse.php
+++ branches/1.05/swisscenter/music_browse.php
@@ -10,10 +10,11 @@
 
   function output_link( $file )
   {
-    return pl_link('file',$file);
+    $id = db_value("select file_id from mp3s where concat(dirname,filename) = '".db_escape_str($file)."'");   
+    return pl_link('sql','select * from mp3s where file_id='.$id,'audio');
   }
 
-  browse_fs('Browse Music', $_SESSION["opts"]["dirs"]["music"], 'music.php', array('mp3'), 'music_select_all.php?', 'select filename,dirname from mp3s where dirname' );
+  browse_fs('Browse Music', $_SESSION["opts"]["dirs"]["music"], 'music.php', array('mp3'), '', 'select filename,dirname from mp3s where dirname' );
 
 /**************************************************************************************************
                                                End of file
