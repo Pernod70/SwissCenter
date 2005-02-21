a8caf8562ce8c34d111bc9e2ea69354f558d8601
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2005-02-21 13:42:33 +0000

    - Removed the following member variables (and associated functions/references):   - bar_height (bar height is automatic)   - icon_width (now read from the image file)   - icon_height (now read from the image file)
    - Displays some text next to the icon (to help the user work out what it is suppossed to be!)

--- branches/1.05/swisscenter/base/iconbar.php
+++ branches/1.05/swisscenter/base/iconbar.php
@@ -6,72 +6,60 @@
 class iconbar
 {
 
+  // -------------------------------------------------------------------------------------------------
   // Member variables
+  // -------------------------------------------------------------------------------------------------
+
   var $icons = array();
   var $bar_width = 400;
-  var $bar_height = 30;
   
-  var $icon_width = 30;
-  var $icon_height = 30;
-
+  // -------------------------------------------------------------------------------------------------
   // Get/set methods
+  // -------------------------------------------------------------------------------------------------
+
   function set_bar_width($width)
   {
     $this->bar_width = $width;
   }
-  
-  function set_bar_height($height)
-  {
-    $this->bar_height = $height;
-  }
-  
-  function set_icon_width($width)
-  {
-    $this->icon_width = $width;
-  }
-  
-  function set_icon_height($height)
-  {
-    $this->icon_heigth = $height;
-  }
-  
-  
+    
+  // -------------------------------------------------------------------------------------------------
   // Constructor
-  function iconbar($width = 400, $height = 30)
+  // -------------------------------------------------------------------------------------------------
+
+  function iconbar($width = 400)
   {
     $this->bar_width = $width;
-    $this->bar_height = $height;
   }
   
+  // -------------------------------------------------------------------------------------------------
   // Methods
-  function add_icon($img_url, $link_url = "")
+  // -------------------------------------------------------------------------------------------------
+
+  function add_icon($img_name, $text, $link_url = "")
   {
-    $this->icons[] = array( "img"=>$img_url, "link"=>$link_url );
+    $filename = style_img($img_name);
+    $imagedata  = getimagesize(style_img($img_name,true));
+    $this->icons[] = array( "img"=>$filename, "text" => $text, "link"=>$link_url, "width" =>$imagedata[0], "height"=>$imagedata[1] );
   }
   
   function display()
   {
-    echo '<table border="0" align="center" width="'.$this->bar_width.'px" height="'.$this->bar_height.'px">';
+    echo '<table border="0" align="center" width="'.$this->bar_width.'px" >';
     echo '<tr>';
     
     foreach($this->icons as $icon)
     {
-      $width = $this->icon_width;
-      $height = $this->icon_height;
-//      image_resized_xy(style_img("ICON_".$icon["img"]), $width, $height);
-      
-      $height = 32;
+      $width  = $this->icon_width;
+      $height = $this->icon_height;      
 
-      echo '<td align="center" height="'.$height.'"width="'.$bar_width / count($this->icons).'px">';
-      
-      if(!empty($icon["link"]))
-        echo '<a href="'.$icon["link"].'">';
-
-      echo '<img border="0" src="'.style_img("ICON_".$icon["img"]).'" height="'.$height.'" width="'.$width.'">';
+      echo '<td valign=middle align="center" width="'.$this->bar_width / count($this->icons).'px">
+            <img align="absmiddle" border="0" src="'.$icon["img"].'" height="'.$icon["height"].'" width="'.$icon["width"].'">&nbsp;';
 
       if(!empty($icon["link"]))
-        echo '</a>';
-        
+        echo '<a href="'.$icon["link"].'"><font size="1">'.$icon["text"].'</font></a>';
+      else 
+        echo '<font size="1">'.$icon["text"].'</font>';
+
       echo '</td>';
     }
     
