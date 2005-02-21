8bdb438459279fbd981ee05dd6dec959ec2182ec
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2005-02-21 14:05:44 +0000

    Determines the format of the source image, and returns the resize image in the same format.

--- branches/1.05/swisscenter/thumb.php
+++ branches/1.05/swisscenter/thumb.php
@@ -12,6 +12,7 @@
 
   // Parameters to the script. Need to do more extensive checking on them!
   $filename   = un_magic_quote(rawurldecode($_REQUEST["src"]));
+  $format     = strtolower(file_ext($filename));
   $x          = $_REQUEST["x"];
   $y          = $_REQUEST["y"];
   $cache_file = str_suffix($_SESSION["opts"]["cache_dir"],'/').'SwissC'.md5($filename.'_x'.$x.'y'.$y).'.png';
@@ -82,8 +83,22 @@
       imagecopyResampled ($im2, $image, 0, 0, 0, 0, $x, $y, $imagedata[0], $imagedata[1]);
   
       // Output the image to the browser
-      header("Content-type: image/png");
-      ImagePng($im2);
+      switch ($format)
+      {
+        case 'jpg':
+        case 'jpeg':
+              header("Content-type: image/jpeg");
+              ImageJpeg($im2);
+              break;
+        case 'gif':
+              header("Content-type: image/gif");
+              ImageGif($im2);
+              break;
+        case 'png';
+              header("Content-type: image/png");
+              ImagePng($im2);
+              break;
+      }
   
       // If a cache directory has been defined, then store the cached file into it.
       if (!empty($_SESSION["opts"]["cache_dir"]))
