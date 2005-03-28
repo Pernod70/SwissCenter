406f07487654a27ecdf933944f3ae56c01a63a39
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2005-03-28 12:04:35 +0000

    Eliminates duplicate rows and then enforces a unique index on (dirname,filename)

--- branches/development/swisscenter/database/update_106.sql
+++ branches/development/swisscenter/database/update_106.sql
@@ -50,6 +50,42 @@ ALTER TABLE media_locations ADD COLUMN (cat_id INT UNSIGNED NOT NULL DEFAULT 1);
 
 ALTER TABLE media_locations ADD FOREIGN KEY (cat_id) REFERENCES categories (cat_id) ON DELETE SET DEFAULT;
 
+-- -------------------------------------------------------------------------------------------------
+-- Remove duplicate entries from the media tables and then enforce DIRNAME and FILENAME to be unique
+-- -------------------------------------------------------------------------------------------------
+
+-- Create temporary tables that identify duplicate rows that are to be deleted.
+
+CREATE TEMPORARY TABLE mp3s_del AS 
+    SELECT max(file_id) file_id 
+      FROM mp3s 
+  GROUP BY dirname,filename 
+    HAVING count(*)>1;
+
+CREATE TEMPORARY TABLE movies_del AS 
+    SELECT max(file_id) file_id 
+      FROM movies
+  GROUP BY dirname,filename 
+    HAVING count(*)>1;
+
+CREATE TEMPORARY TABLE photos_del AS 
+    SELECT max(file_id) file_id 
+      FROM photos
+  GROUP BY dirname,filename 
+    HAVING count(*)>1;
+
+-- Delete the rows identified as duplicates
+
+DELETE FROM mp3s   USING mp3s, mp3s_del     WHERE mp3s.file_id = mp3s_del.file_id;
+DELETE FROM movies USING movies, movies_del WHERE movies.file_id = movies_del.file_id;
+DELETE FROM photos USING photos, photos_del WHERE photos.file_id = photos_del.file_id;
+
+-- Create Unique indexes
+
+CREATE UNIQUE INDEX mp3s_fsp_u1   ON mp3s (dirname(800),filename(200));
+CREATE UNIQUE INDEX movies_fsp_u1 ON movies (dirname(800),filename(200));
+CREATE UNIQUE INDEX photos_fsp_u1 ON photos (dirname(800),filename(200));
+
 -- *************************************************************************************************
 --   SWISScenter Source                                                              Robert Taylor
 -- *************************************************************************************************
