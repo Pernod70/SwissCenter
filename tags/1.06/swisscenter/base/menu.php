e41b9a99c099fff7f67f226c5cd4bc2b492de913
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2005-04-15 15:23:33 +0000

    made a copy

---
+++ tags/1.06/swisscenter/base/menu.php
@@ -0,0 +1,106 @@
+<?
+/**************************************************************************************************
+   SWISScenter Source                                                              Robert Taylor
+ *************************************************************************************************/
+
+ob_start();
+
+require_once("settings.php");
+require_once("utils.php");
+require_once("page.php");
+
+//
+// Class for outputting menus.
+//
+
+class menu
+{
+
+  #-------------------------------------------------------------------------------------------------
+  # Member Variables
+  #-------------------------------------------------------------------------------------------------
+  var $menu_items;
+  var $up;
+  var $down;
+  var $icons = true;
+
+  #-------------------------------------------------------------------------------------------------
+  # Member Functions
+  #-------------------------------------------------------------------------------------------------
+
+  function menu( $show_icons = true)
+  {
+    $this->icons = $show_icons;
+  }
+
+  function add_item( $text, $url="", $right=false )
+  {
+    if (! is_null($text) && !empty($text))
+      $this->menu_items[] = array( "text"=>$text, "url"=>$url,
+                                   "right"=> ($right == true ? '</td><td><img src="'.style_img("IMG_RIGHT").'">' : '') );
+  }
+
+  function add_up( $url )
+  {
+    $this->up = $url;
+  }
+
+  function add_down( $url )
+  {
+    $this->down = $url;
+  }
+
+  function display( $size=400 )
+  {
+    $i=0;
+    $link="";
+
+    echo '<center><table cellspacing="3" cellpadding="3" border="0">';
+
+    if ($this->icons )
+    {
+      if (! empty($this->up))
+        echo '<tr><td align="center" valign="middle" width="'.$size.'px" height="10px">'.
+             up_link($this->up).
+             '</td></tr>';
+      else
+        echo '<tr><td align="center" valign="middle" width="'.$size.'px" height="10px"></a></td></tr>';
+    }
+
+    if (! empty($this->menu_items))
+    {
+      foreach ($this->menu_items as $item)
+      {
+        $text = shorten($item["text"],$size - 50);
+
+        $link = $item["url"];
+        if (substr($link,0,5) != 'href=')
+          $link = 'href="'.$link.'"';
+          
+        if ( style_img_exists("IMG_MENU_".$size) )
+          $menu_bg_img = style_img("IMG_MENU_".$size);
+        else 
+          $menu_bg_img = style_img("IMG_MENU");
+
+        echo '<tr><td valign="middle" width="'.$size.'px" height="25px" background="'.$menu_bg_img.'">&nbsp;&nbsp;&nbsp;'.
+              '<font color="'.style_value("TITLE_COLOUR",'#FFFFFF').'">'.++$i.'.</font> <a '.$link.' TVID="'.$i.
+              '" name="'.$i.'">'.$text.'</a>'.$item["right"].'</td></tr>';
+      }
+    }
+
+    if ($this->icons && !empty($this->down))
+    {
+        echo '<tr><td align="center" valign="middle" width="'.$size.'px" height="10px">'.
+             down_link($this->down).
+             '</td></tr>';
+    }
+
+    echo '</table></center>';
+  }
+
+}
+
+/**************************************************************************************************
+                                               End of file
+ **************************************************************************************************/
+?>
---
+++ tags/1.06/swisscenter/music.php
@@ -0,0 +1,48 @@
+<?
+/**************************************************************************************************
+   SWISScenter Source                                                              Robert Taylor
+ *************************************************************************************************/
+
+  require_once("base/page.php");
+  require_once("base/categories.php");
+
+  function display_music_menu($cat_id)
+  {
+    if(empty($cat_id))
+      $_SESSION["history"] = array(array("url"=>"music.php"));
+    else
+      $_SESSION["history"] = array(array("url"=>"music.php?cat=$cat_id", "sql"=>category_select_sql($cat_id, 1)));
+
+    echo '<center>Please select an option from the list:</center><p>';
+
+    $menu = new menu();
+    $menu->add_item("Browse Music by Artist Name","music_search.php?sort=artist",true);
+    $menu->add_item("Browse Music by Album Name","music_search.php?sort=album",true);
+    $menu->add_item("Browse Music by Track Name","music_search.php?sort=title",true);
+    $menu->add_item("Browse Music by Genre","music_search.php?sort=genre",true);
+    $menu->add_item("Browse Music by Year","music_search.php?sort=year",true);
+    $menu->add_item("Browse Filesystem","music_browse.php",true);
+    $menu->display();
+    
+    if(!empty($cat_id))
+      page_footer('music.php');
+    else
+      page_footer('index.php');
+  }
+
+/**************************************************************************************************
+   Main page output
+   *************************************************************************************************/
+
+  page_header("Listen to Music", '', 'LOGO_MUSIC');
+  $cat_id = $_REQUEST["cat"];
+  
+  if( !empty($cat_id) )
+    display_music_menu($cat_id);
+  else
+    display_categories('music.php', 1);
+
+/**************************************************************************************************
+                                               End of file
+ **************************************************************************************************/
+?>
