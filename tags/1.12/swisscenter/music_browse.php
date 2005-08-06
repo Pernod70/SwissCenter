968fa2f0835f39ac7c5ed39219472c1e7e2080c2
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2005-08-06 10:49:50 +0000

    made a copy

---
+++ tags/1.12/swisscenter/music_browse.php
@@ -0,0 +1,31 @@
+<?
+/**************************************************************************************************
+   SWISScenter Source                                                              Robert Taylor
+ *************************************************************************************************/
+
+  require_once("base/page.php");
+  require_once("base/utils.php");
+  require_once('base/browse.php');
+  require_once('base/playlist.php');
+  require_once('base/rating.php');
+
+  function output_link( $file )
+  {
+    $id = db_value("select file_id from mp3s where concat(dirname,filename) = '".db_escape_str($file)."'");   
+    return pl_link('sql','select * from mp3s where file_id='.$id,'audio');
+  }
+
+  $sql = 'from mp3s media'.get_rating_join().'where 1=1';
+
+  browse_db( 'Browse Music'
+           , db_col_to_list("select name from media_locations where media_type=1")
+           , $sql.$_SESSION["history"][0]["sql"]
+           , $_SESSION["history"][0]["url"]
+           , array('mp3')
+           , ''
+           , 'LOGO_MUSIC' );
+
+/**************************************************************************************************
+                                               End of file
+ **************************************************************************************************/
+?>
