66942c86a745da09a34be87470516cf0580d2491
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2006-02-21 19:33:54 +0000

    Moved remotely

---
+++ 1.14/swisscenter/base/server.php
@@ -0,0 +1,129 @@
+<?
+/**************************************************************************************************
+   SWISScenter Source                                                              Robert Taylor
+ *************************************************************************************************/
+   
+  // ----------------------------------------------------------------------------------
+  // Returns the Operating System type
+  // ----------------------------------------------------------------------------------
+  
+  function get_os_type()
+  {
+    if ( substr(PHP_OS,0,3)=='WIN' )
+      return 'WINDOWS';
+    else
+      return 'UNIX';
+  }
+  
+  function is_windows()
+  { return get_os_type() == "WINDOWS"; }
+  
+  function is_unix()
+  { return get_os_type() == "UNIX"; } 
+  
+  // ----------------------------------------------------------------------------------
+  // Returns the webserver type
+  // ----------------------------------------------------------------------------------
+  
+  function get_server_type()
+  {
+    $server_type = "UNKNOWN";
+    
+    if(strpos($_SERVER["SERVER_SOFTWARE"], "Apache") !== false )
+      $server_type = "APACHE";
+    elseif(strpos($_SERVER["SERVER_SOFTWARE"], "IIS") !== false)
+      $server_type = "IIS";
+    elseif(strpos($_SERVER["SERVER_SOFTWARE"], "Simese") !== false)
+      $server_type = "SIMESE";
+    else
+      $server_type = "SIMESE";
+    
+    return $server_type;
+  }
+  
+  function is_server_iis()
+  { return get_server_type() == "IIS"; }
+  
+  function is_server_apache()
+  { return get_server_type() == "APACHE"; }
+  
+  function is_server_simese()
+  { return get_server_type() == "SIMESE"; }
+  
+  function simese_version()
+  {
+    if ( is_server_simese() )
+      return substr($_SERVER["SERVER_SOFTWARE"],7);
+    else 
+      return false;
+  }
+
+  // ----------------------------------------------------------------------------------
+  // Returns the full URL (SCRIPT_NAME + QUERY_STRING) of the current page
+  // ----------------------------------------------------------------------------------
+  
+  function current_url()
+  {
+    if(is_server_apache() || is_server_iis())
+      return "http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
+    else 
+      return $_SERVER["SCRIPT_NAME"].(empty($_SERVER["QUERY_STRING"]) ? "" : "?".$_SERVER["QUERY_STRING"]);
+  }
+
+  // ----------------------------------------------------------------------------------
+  // Returns the IP address of the Client
+  // ----------------------------------------------------------------------------------
+
+  function client_ip()
+  {
+    return str_replace('\\','/',$_SERVER["REMOTE_ADDR"]);
+  }  
+
+  // ----------------------------------------------------------------------------------
+  // Returns the HTTP address (IP and Port) where the SwissCenter is installed.
+  // ----------------------------------------------------------------------------------
+
+  function server_address()
+  {
+    if (is_server_iis())
+      return 'http://'.$_SERVER['LOCAL_ADDR'].":".$_SERVER['SERVER_PORT'].'/';
+    else
+      return 'http://'.$_SERVER['SERVER_ADDR'].":".$_SERVER['SERVER_PORT'].'/';
+  }
+
+  // ----------------------------------------------------------------------------------
+  // Returns TRUE if the server has internet connectivity
+  // ----------------------------------------------------------------------------------
+
+  function internet_check( $timeouts = 3)
+  {
+    $temp = '';
+
+    for ($i=0; $i < $timeouts; $i++)
+      if ( $sock = @fsockopen('66.249.87.99', 80, $temp, $temp, 0.5)) // www.google.com
+      {
+        fclose($sock);
+        return 'YES'; 
+      }
+
+    return 'NO';
+  }
+  
+  function internet_available()
+  {
+    if ( isset($_SESSION["internet"]) && !is_array($_SESSION["internet"]))
+      $_SESSION=array();
+    
+    if ( !isset($_SESSION["internet"]) || $_SESSION["internet"]["timeout"] < time() )
+    {
+      $_SESSION["internet"]["available"] = internet_check();
+      $_SESSION["internet"]["timeout"]   = time()+900; // 15 mins
+    }
+    
+    return ( $_SESSION["internet"]["available"] == 'YES' );
+  }
+  
+/**************************************************************************************************
+                                               End of file
+ **************************************************************************************************/
+?>
