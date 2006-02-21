c8958eb30c136d5df728f0b3e45d2c08bb636138
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2006-02-21 19:34:03 +0000

    Moved remotely

---
+++ 1.06/swisscenter/base/stylelib.php
@@ -0,0 +1,84 @@
+<?
+/**************************************************************************************************
+   SWISScenter Source                                                              Robert Taylor
+ *************************************************************************************************/
+
+//-------------------------------------------------------------------------------------------------
+// This procedure stores the STYLE information in the current session. 
+//-------------------------------------------------------------------------------------------------
+
+function load_style()
+{
+  $style = db_value("select value from user_prefs where user_id=".CURRENT_USER." and name='STYLE'");
+  
+  if (!empty($style) && file_exists(SC_LOCATION.'styles/'.$style.'/style.ini'))
+  {
+    $details = parse_ini_file(SC_LOCATION.'styles/'.$style.'/style.ini');
+    $details["location"] = '/styles/'.$style.'/';
+    $details["name"]     = $style;
+  }
+  else 
+  {
+    $details = parse_ini_file(SC_LOCATION.'images/style.ini');
+    $details["location"] = '/images/';
+    $details["name"]     = 'Default';
+  }
+  
+  // Ensure the display for audio will be in the correct place...
+  if (get_screen_type() == 'PAL')
+    $dummy = @file_get_contents('http://'.client_ip().':2020/pod_audio_info.cgi?x=210&y=464');  
+  else
+    $dummy = @file_get_contents('http://'.client_ip().':2020/pod_audio_info.cgi?x=210&y=369');  
+         
+  // Cache the style parameters in the session
+  $_SESSION["style"] = $details;
+}
+ 
+//-------------------------------------------------------------------------------------------------
+// Returns a given image paramter from the Style settings.
+//-------------------------------------------------------------------------------------------------
+
+function style_img ($name, $ext_url = false)
+{
+  if ( !isset($_SESSION["style"]))
+    load_style();
+    
+  $path   = substr(SC_LOCATION,0,-1);
+  $file   = $_SESSION["style"]["location"].$_SESSION["style"][strtoupper($name)];
+
+  if ( isset( $_SESSION["style"][strtoupper($name)]) && file_exists($path.$file) )
+    return ($ext_url ? $path : '').$file;
+  else 
+    return ($ext_url ? $path : '').'/images/dot.gif';
+}
+
+function style_img_exists ($name)
+{
+  if ( !isset($_SESSION["style"]))
+    load_style();
+
+  $path   = substr(SC_LOCATION,0,-1);
+  $file   = $_SESSION["style"]["location"].$_SESSION["style"][strtoupper($name)];
+
+  if ( isset( $_SESSION["style"][strtoupper($name)]) && file_exists($path.$file) )
+    return true;
+  else 
+    return false;
+}
+
+function style_value ( $name, $default = '')
+{
+  if ( !isset($_SESSION["style"]))
+    load_style();
+
+  if ( isset( $_SESSION["style"][strtoupper($name)]) && !empty($_SESSION["style"][strtoupper($name)]) )
+    return $_SESSION["style"][strtoupper($name)];
+  else
+    return $default;
+}
+
+   
+/**************************************************************************************************
+                                               End of file
+ **************************************************************************************************/
+?>
---
+++ 1.06/swisscenter/video_search.php
@@ -0,0 +1,111 @@
+<?
+/**************************************************************************************************
+   SWISScenter Source                                                              Robert Taylor
+ *************************************************************************************************/
+
+  require_once("base/page.php");
+  require_once("base/utils.php");
+  require_once("base/mysql.php");
+  require_once("base/az_picker.php");
+
+  function search_page( $title, $column, $sort, $this_url, $prefix, $search, $page)
+  {
+    page_header('Video', $title.' : '.$search,'LOGO_MOVIE','', (empty($_REQUEST["last"]) ? 'KEY_SPC' : $_REQUEST["last"] ) );
+
+    $menu      = new menu();
+    $data      = array();
+    $back_url  = $_SESSION["history"][count($_SESSION["history"])-1]["url"];
+    $post_sql  = $_SESSION["history"][count($_SESSION["history"])-1]["sql"];
+    $sql       = "from movies m 
+                  left outer join directors_of_movie dom on m.file_id = dom.movie_id
+                  left outer join genres_of_movie gom on m.file_id = gom.movie_id
+                  left outer join actors_in_movie aim on m.file_id = aim.movie_id
+                  left outer join actors a on aim.actor_id = a.actor_id
+                  left outer join directors d on dom.director_id = d.director_id
+                  left outer join genres g on gom.genre_id = g.genre_id where ";
+
+    echo '<table border=0 height="320px" width="100%"><tr><td width="200px" valign="top">';
+    show_picker( $this_url.'?sort='.$sort.'&any='.$prefix.'&search=', $search);
+    echo '</td><td valign=top>';
+
+    // Get the matching records from the database.
+    $data = db_toarray("select distinct $column display $sql $column !='' and $column like '".$prefix.db_escape_str(str_replace('_','\_',$search))."%' ".$post_sql." order by 1 limit ".(($page*MAX_PER_PAGE)).",".MAX_PER_PAGE);
+    $num_rows = db_value("select count(distinct $column) $sql $column !='' and $column like '".$prefix.db_escape_str(str_replace('_','\_',$search))."%' ".$post_sql);
+
+    if ( $data === false || $num_rows === false)
+      page_error('A database error occurred');
+
+    if (empty($data))
+    {
+      echo 'There are no items that match your search.';
+    }
+    else
+    {
+
+      // We are not on the first page, so output a link to go "up" a page of entries.
+      if ($page > 0)
+        $menu->add_up( $this_url.'?sort='.$sort.'&search='.rawurlencode($search).'&any='.$prefix.'&page='.($page-1));
+
+      // We are not on the last page, so output a link to go "down" a page of entries.
+      if (($page+1)*MAX_PER_PAGE < $num_rows)
+        $menu->add_down( $this_url.'?sort='.$sort.'&search='.rawurlencode($search).'&any='.$prefix.'&page='.($page+1));
+
+      foreach ($data as $row)
+      {
+        $menu->add_item($row["DISPLAY"],'video_selected.php?add=Y&type='.$sort.'&name='.rawurlencode($row["DISPLAY"]),true);
+      }
+      $menu->display( "300" );
+    }
+
+    echo '</td></tr></table>';
+
+    // Output ABC buttons if appropriate
+
+    if (empty($prefix))
+      $buttons[] = array('text'=>'Anywhere in Name', 'url'=>$this_url.'?sort='.$sort.'&search='.rawurlencode($search).'&any=%' );
+    else
+      $buttons[] = array('text'=>'Start of Name', 'url'=>$this_url.'?sort='.$sort.'&search='.rawurlencode($search).'&any=' );
+
+    $buttons[] = array('text'=>'Clear Search', 'url'=>$this_url.'?sort='.$sort.'&any='.$prefix);
+    $buttons[] = array('text'=>'Select All', 'url'=>'video_selected.php?add=Y&type='.$sort.'&name='.rawurlencode($search.'%'));
+
+    page_footer($back_url, $buttons);
+  }
+
+//*************************************************************************************************
+// Main Code
+//*************************************************************************************************
+
+  // Should we delete the last entry on the history stack?
+  if (strtoupper($_REQUEST["del"]) == 'Y')
+    array_pop($_SESSION["history"]);
+
+    // Check page parameters, and if not set then assign default values.
+  $sort    = $_REQUEST["sort"];
+  $search  = un_magic_quote(rawurldecode($_REQUEST["search"]));
+  $prefix  = $_REQUEST["any"];
+  $page    = ( empty($_REQUEST["page"]) ? 0 : $_REQUEST["page"]);
+  
+  switch ($sort)
+  {
+    case "title":
+    case "year":
+      $column = $sort;
+      break;
+    case "genre":
+    case "actor":
+    case "director":
+      $column = $sort."_name";
+      break;
+    case "certificate":
+      $column = "rating";
+      break;
+  }
+
+  search_page( 'Browse by '.ucfirst($sort), $column, $sort, 'video_search.php', $prefix, $search, $page)
+
+
+/**************************************************************************************************
+                                               End of file
+ **************************************************************************************************/
+?>
