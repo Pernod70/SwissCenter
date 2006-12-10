951bf75355d8fdcb4d72e0b249ddc13366ff745f
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2006-12-10 20:24:06 +0000

    Copied remotely

---
+++ 1.15.4/swisscenter/photo_search.php
@@ -0,0 +1,31 @@
+<?
+/**************************************************************************************************
+   SWISScenter Source                                                              Robert Taylor
+ *************************************************************************************************/
+
+  require_once( realpath(dirname(__FILE__).'/base/search.php'));
+
+  // Check page parameters
+  $column        = $_REQUEST["sort"];
+  $joined_tables = "inner join photo_albums pa on media.dirname like concat(pa.dirname,'%') ";
+
+  switch ($column)
+  {
+    case "filename":
+        $title       = str('PHOTO_TITLE');
+        break;
+    case "title":
+        $title       = str('PHOTO_ALBUM');
+        break;
+    default :
+        send_to_log(1,'Unknown $column in photo_album_search.php');
+        page_error('Unexpected error - please see log for details');
+        break;
+  }
+
+  search_media_page( str('VIEW_PHOTO'), $title, 'photos', $joined_tables, $column, 'photo_selected.php' );
+
+/**************************************************************************************************
+                                               End of file
+ **************************************************************************************************/
+?>
