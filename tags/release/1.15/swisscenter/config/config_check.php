ab826dee63783beee0ae9f4111a0ddfe8dc3e72e
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2006-12-10 20:20:37 +0000

    Copied remotely

---
+++ tags/release/1.15/swisscenter/config/config_check.php
@@ -0,0 +1,266 @@
+<?
+/**************************************************************************************************
+   SWISScenter Source                                                              Robert Taylor
+ *************************************************************************************************/
+
+require_once( realpath(dirname(__FILE__).'/../base/sched.php'));
+require_once( realpath(dirname(__FILE__).'/../base/install_checks.php'));
+
+// ----------------------------------------------------------------------------------
+// A class to output the test results onto the page in a pretty format!
+// ----------------------------------------------------------------------------------
+
+class test_results
+{
+  #-------------------------------------------------------------------------------------------------
+  # Member Variables
+  #-------------------------------------------------------------------------------------------------
+
+  var $sections;
+  var $web_results;
+  var $cli_results;
+  
+  #-------------------------------------------------------------------------------------------------
+  # Constructor
+  #-------------------------------------------------------------------------------------------------
+
+  function test_results()
+  {    
+    $this->sections    = array ();
+    $this->cli_results = array();
+    
+    // Get the results of all the installation tests when run via the webserver
+    $this->web_results = get_check_results();
+
+    // Simese hangs when you attempt to run a PHP script in the foreground, but we don't really need to
+    // run the tests under the CLI version of PHP as that's how simese operates normally anyway.
+    if (! is_server_simese())
+    {
+      // Now run the same tests again via the command line.
+      // This will reveal any differences between the webserver install (which might be using mod_php) 
+      // and the command line version - for instance, they might be on totally difference versions.
+      $cli = run_foreground('config/cli_test.php');
+      if ($cli !== false)
+      {
+        $cli = explode("\n",str_replace("\r",'',$cli));
+        $cli = unserialize(implode("\n",array_splice($cli,array_search("",$cli)+1)));
+        $this->cli_results = $cli;
+      }
+    }
+  }
+
+  #-------------------------------------------------------------------------------------------------
+  # Add a new section and returns the identifier for it (used when adding tests).
+  #-------------------------------------------------------------------------------------------------
+
+  function add_section( $heading )
+  {
+    $this->sections[] = array( "heading" => $heading
+                             , "passed" => array()
+                             , "failed_cli" => array() 
+                             , "failed_web" => array() 
+                             );
+    return count($this->sections)-1;
+  }
+  
+  #-------------------------------------------------------------------------------------------------
+  # Returns the result of a test (true for pass, false for fail)
+  #-------------------------------------------------------------------------------------------------
+
+  function test_result( $key )
+  {
+    if ( key_exists($key,$this->web_results) )
+    {      
+      if (!$this->web_results[$key] || (count($this->cli_results)>0 && !$this->cli_results[$key]) )
+        return false;
+      else 
+        return true;
+    }
+    else 
+      return false;
+  }
+  
+  #-------------------------------------------------------------------------------------------------
+  # Add details about a specific test
+  #-------------------------------------------------------------------------------------------------
+
+  function add_test( $section, $key, $pass, $fail)
+  {
+    if ( key_exists($section,$this->sections) )
+    {      
+      if (!$this->web_results[$key])
+        $this->sections[$section]["failed_web"][] = $fail;
+      elseif (count($this->cli_results)>0 && !$this->cli_results[$key])
+        $this->sections[$section]["failed_cli"][] = $fail;
+      else 
+        $this->sections[$section]["passed"][] = $pass;
+    }
+  }
+  
+  #-------------------------------------------------------------------------------------------------
+  # Display the results
+  #-------------------------------------------------------------------------------------------------
+
+  function display()
+  {
+    echo '<h1>'.str('INSTALL_TEST_TITLE').'</h1>
+          <p><center>';
+    foreach ($this->sections as $section)
+    {
+      $failures = (count($section["failed_cli"]) + count($section["failed_web"]) > 0);
+      
+      // Show the heading for this section (complete with pass/fail icon)
+      echo '<p><table border=1 noshade width="90%" cellspacing=0 cellpadding=8 bgcolor="#ffffff">
+            <tr>
+              <td width="100%"><b>'.$section["heading"].'</b></td>
+              <td width="40" valign=top align=center><img src="'.($failures ? 'fail.png' : 'pass.png').'"></td>
+            </tr>
+            <tr><td colspan=2>';
+      
+      if (count($section["passed"]) > 0)
+      {
+        echo '<p>'.str('INSTALL_TEST_SUCCESS').'<font color="#006600"><ul>';
+        foreach ($section["passed"] as $msg)
+          echo '<li>'.$msg;
+        echo '</ul></font>';
+      }
+      
+      if (count($section["failed_web"]) > 0)
+      {
+        echo '<p>'.str('INSTALL_TEST_FAIL_WEB').'<font color="#660000"><ul>';
+        foreach ($section["failed_web"] as $msg)
+          echo '<li>'.$msg;
+        echo '</ul></font>';
+      }
+
+      if (count($section["failed_cli"]) > 0)
+      {
+        echo '<p>'.str('INSTALL_TEST_FAIL_CLI').'<font color="#660000"><ul>';
+        foreach ($section["failed_cli"] as $msg)
+          echo '<li>'.$msg;
+        echo '</ul></font>';
+      }
+
+      // End the display for this section
+      echo '</td>
+            </tr>
+            </table>';
+    }
+    echo '</center>';
+  }
+
+}
+
+// ----------------------------------------------------------------------------------
+// Display test results
+// ----------------------------------------------------------------------------------
+
+function check_display()
+{
+  load_lang();
+  $test_page = new test_results();
+    
+  # ----------------------
+  # PHP Tests
+  # ----------------------
+  
+  $php = $test_page->add_section("PHP");
+  
+  if (! is_server_simese() || version_compare(simese_version(),'1.31','<') )
+  {
+    $test_page->add_test( $php, "PHP cli"
+                        , str("PASS_PHP_CLI")
+                        , str("FAIL_PHP_CLI" ) );    
+  }
+
+  $test_page->add_test( $php, "PHP version"
+                      , str("PASS_PHP_VERSION")
+                      , str("FAIL_PHP_VERSION",phpversion()) );
+
+  $test_page->add_test( $php, "PHP ini file"
+                      , str("PASS_PHP_INI")
+                      , str("FAIL_PHP_INI"));
+
+  $test_page->add_test( $php, "PHP required mods"
+                      , str("PASS_PHP_REQ_MODS")
+                      , str("FAIL_PHP_REQ_MODS", implode(', ',get_required_modules_list())) );
+
+  $test_page->add_test( $php, "PHP suggested mods"
+                      , str("PASS_PHP_EXTRA_MODS")
+                      , str("FAIL_PHP_EXTRA_MODS", implode(', ',get_suggested_modules_list())) );
+
+  # ----------------------
+  # MySQL Tests
+  # ----------------------
+  
+  # If there is no swisscenter.ini file present, then we will not be able to connect to the database
+  # because that is where the connection details are stored.
+  
+  if ( $test_page->test_result('SWISS ini file'))
+  {                         
+    $mysql = $test_page->add_section("MySQL");
+  
+    $test_page->add_test( $mysql, "MYSQL connect"
+                        , str("PASS_MYSQL_CONNECT")
+                        , str("FAIL_MYSQL_CONNECT"));
+  
+    $test_page->add_test( $mysql, "MYSQL version"
+                        , str("PASS_MYSQL_VERSION")
+                        , str("FAIL_MYSQL_VERSION"));
+  
+    $test_page->add_test( $mysql, "MYSQL database"
+                        , str("PASS_MYSQL_DB")
+                        , str("FAIL_MYSQL_DB"));
+  }
+                      
+  # ----------------------
+  # SwissCenter configuration Tests
+  # ----------------------
+                           
+  $swiss = $test_page->add_section("SwissCenter");
+
+  // It only makes sense to check for root installations on UNIX.
+  if (is_unix())
+  {
+    $test_page->add_test( $swiss, "SWISS root install"
+                        , str("PASS_SWISS_ROOT_INS")
+                        , str("ROOT_INSTALL_TEXT"));
+  }
+
+  $test_page->add_test( $swiss, "SWISS write root"
+                      , str("PASS_SWISS_RW_FILES")
+                      , str("MISSING_PERMS_TEXT"));
+
+  $test_page->add_test( $swiss, "SWISS ini file"
+                      , str("PASS_SWISS_INI")
+                      , str("FAIL_SWISS_INI"));
+
+  $test_page->add_test( $swiss, "SWISS write log"
+                      , str("PASS_SWISS_LOG")
+                      , str("FAIL_SWISS_LOG", logfile_location()) );
+                      
+  if ( $test_page->test_result('MYSQL database'))
+  {                         
+    $test_page->add_test( $swiss, "SWISS media locs"
+                        , str("PASS_SWISS_LOCS")
+                        , str("FAIL_SWISS_LOCS"));
+  }
+
+  # ----------------------
+  # Webserver Tests
+  # ----------------------
+
+  $server = $test_page->add_section("Webserver");
+
+  $test_page->add_test( $server, "SERVER scheduler"
+                      , str("PASS_SERVER_SCHED")
+                      , str("FAIL_SERVER_SCHED"));
+                           
+  # Display test results
+  $test_page->display();
+}
+
+/**************************************************************************************************
+                                               End of file
+ **************************************************************************************************/
+?>
---
+++ tags/release/1.15/swisscenter/media_search.php
@@ -0,0 +1,57 @@
+<?
+/**************************************************************************************************
+   SWISScenter Source                                                              Robert Taylor
+ *************************************************************************************************/
+
+  require_once( realpath(dirname(__FILE__).'/base/settings.php'));
+  require_once( realpath(dirname(__FILE__).'/base/page.php'));
+  require_once( realpath(dirname(__FILE__).'/base/mysql.php'));
+  require_once( realpath(dirname(__FILE__).'/base/utils.php'));
+  require_once( realpath(dirname(__FILE__).'/base/media.php'));
+  require_once( realpath(dirname(__FILE__).'/video_obtain_info.php'));
+
+  set_time_limit(0);
+  ini_set('memory_limit',-1);
+  $start_time = time();
+  $cache_dir = get_sys_pref('cache_dir');
+
+  // ----------------------------------------------------------------------------------
+  // Processes each media location in turn, setting the verified flag and deleting
+  // records which are no longer available.
+  // ----------------------------------------------------------------------------------
+
+  function process_media_dirs( $media_type )
+  {
+    $media_locations = db_toarray("select * from media_locations where media_type=$media_type");
+    $table           = db_value("select media_table from media_types where  media_id=$media_type");
+    $types           = media_exts( $media_type );
+
+    send_to_log(4,'Refreshing '.strtoupper($table).' database');
+      
+    foreach ($media_locations as $location)
+      process_media_directory( str_suffix($location["NAME"],'/'), $location["LOCATION_ID"], $table, $types );
+        
+    send_to_log(4,'Completed refreshing '.strtoupper($table).' database');
+  }
+
+  //===========================================================================================
+  // Main script logic
+  //===========================================================================================
+
+  media_indicator('BLINK');
+  remove_orphaned_records();
+  
+  process_media_dirs( MEDIA_TYPE_MUSIC );
+  process_media_dirs( MEDIA_TYPE_VIDEO );
+  process_media_dirs( MEDIA_TYPE_PHOTO );
+
+  if ( is_movie_check_enabled() )
+    extra_get_all_movie_details();
+  
+  eliminate_duplicates();
+  media_indicator('OFF');
+
+/**************************************************************************************************
+                                               End of file
+ **************************************************************************************************/
+?>
