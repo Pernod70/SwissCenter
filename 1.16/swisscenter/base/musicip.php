c38965dfec54bf6f3b4dc27dd5b4669befe933fe
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2007-05-07 19:10:21 +0000

    Copied remotely

---
+++ 1.16/swisscenter/base/musicip.php
@@ -0,0 +1,116 @@
+<?
+/**************************************************************************************************
+   SWISScenter Source                                                              Robert Taylor
+ *************************************************************************************************/
+
+require_once( realpath(dirname(__FILE__).'/prefs.php'));
+require_once( realpath(dirname(__FILE__).'/utils.php'));
+
+// ----------------------------------------------------------------------------------
+// Returns the URL for the MusicIP api
+// ----------------------------------------------------------------------------------
+
+function musicip_address()
+{
+  return 'http://localhost:'.get_sys_pref('MUSICIP_PORT','10002').'/';
+}
+
+// ----------------------------------------------------------------------------------
+// Returns true if a MusicIP webservice is available on the local machine using the
+// port defined in the config screen. The
+// ----------------------------------------------------------------------------------
+
+function musicip_check( $port )
+{
+  $temp = '';
+  $result = false;
+
+  if ( $sock = @fsockopen('localhost', $port , $temp, $temp, 0.5))
+  {
+    fclose($sock);
+    $status = @file_get_contents("http://localhost:$port/api/getstatus");
+    $result = ( $status !== FALSE );
+  }
+
+  return $result;
+}
+
+function musicip_available( $recheck = false)
+{
+  if ( !isset($_SESSION["MUSICIP_AVAILABLE"]) || $recheck )
+    $_SESSION["MUSICIP_AVAILABLE"] = musicip_check( get_sys_pref('MUSICIP_PORT','10002') );
+  return $_SESSION["MUSICIP_AVAILABLE"];
+}
+
+// ----------------------------------------------------------------------------------
+// Returns a link to a MusicIP playlist.
+// ----------------------------------------------------------------------------------
+
+function musicip_mix_link( $type, $value )
+{
+  // Settings for the mix...
+  $params = array( 'content'    => 'm3u'
+                 , 'mixgenre'   => 'false'
+                 , 'size'       => get_sys_pref('MUSICIP_SIZE',20)
+                 , 'sizetype'   => get_sys_pref('MUSICIP_SIZE_TYPE','tracks')
+                 , 'rejectsize' => get_sys_pref('MUSICIP_REJECT',5)
+                 , 'rejecttype' => get_sys_pref('MUSICIP_REJECT_TYPE','tracks')
+                 , 'style'      => get_sys_pref('MUSICIP_STYLE',20)
+                 , 'variety'    => get_sys_pref('MUSICIP_VARIETY',0)
+                 , $type        => urlencode($value)
+                 );
+                       
+  // Save the playlist generating URL into the session for when the playlist is needed.
+  $_SESSION["musicip_playlist"] = url_add_params( musicip_address().'api/mix', $params);
+  
+  // Output a link to play a MusicIP playlist.
+  $params = 'spec_type=musicip&'.current_session().'&seed='.mt_rand(); 
+  $extra = 'pod="'.now_playing_sync_type().',1,'.server_address().'playing_list.php?'.$params.'" ';
+  return 'href="gen_playlist.php?'.$params.'" '.$extra;
+}
+
+function musicip_mix_song( $song )
+{
+  return musicip_mix_link('song',$song);
+}
+
+function musicip_mix_artist( $artist )
+{
+  return musicip_mix_link('artist',$artist);
+}
+
+function musicip_mix_album ( $album )
+{
+  return musicip_mix_link('album',$album);
+}
+
+// ----------------------------------------------------------------------------------
+// Returns a percentage of songs which can be used in a mix out of the total number
+// of available songs.
+// ----------------------------------------------------------------------------------
+
+function musicip_mixable_percent()
+{
+  if ( musicip_available() )
+  {
+    $matches = array();
+    $html = strip_tags(file_get_contents( musicip_address().'server' ));
+    
+    // Total number of songs
+    preg_match_all('/Total Songs *([0-9,]*)/i',$html,$matches);
+    $songs = str_replace(',','',$matches[1][0]);
+
+    // Total number of songs
+    preg_match_all('/Mixable Songs *([0-9,]*)/i',$html,$matches);
+    $mixable = str_replace(',','',$matches[1][0]);
+
+    return (int)($mixable/$songs*100);
+  }
+  else 
+    return false;
+}
+
+/**************************************************************************************************
+                                               End of file
+ **************************************************************************************************/
+?>
---
+++ 1.16/swisscenter/config/config_users.php
@@ -0,0 +1,138 @@
+<?
+/**************************************************************************************************
+   SWISScenter Source                                                              Robert Taylor
+ *************************************************************************************************/
+  
+  // ----------------------------------------------------------------------------------
+  // Display current config
+  // ----------------------------------------------------------------------------------
+  
+  function users_display($modify_msg = '', $add_msg = '', $edit_id = 0)
+  {
+    $data = db_toarray("select user_id, u.Name 'Name', u.Pin, c.name 'Max Certificate Viewable' 
+                          from users u, certificates c 
+                         where u.maxcert=c.cert_id order by u.name asc");
+    
+    
+    echo "<h1>".str('USERS_ADD_TITLE')."</h1>";
+    message($modify_msg);
+    form_start("index.php", 150, "users");
+    form_hidden("section", "USERS");
+    form_hidden("action", "MODIFY");
+    form_select_table("user_id", $data, str('USERS_TABLE_HEADINGS')
+                     ,array("class"=>"form_select_tab","width"=>"100%"), "user_id",
+                      array("NAME"=>"",
+                            "MAX CERTIFICATE VIEWABLE"=>get_cert_list_sql(),
+                            "PIN"=>"*")
+                      , $edit_id, "users");
+    form_submit(str('USERS_DEL_BUTTON'), 1 ,"center");
+    form_end();
+    
+    echo "<p><h1>".str('USERS_ADD_BUTTON')."</h1>";
+    message($add_msg);
+    form_start("index.php", 150);
+    form_hidden("section", "USERS");
+    form_hidden("action", "NEW");
+    form_input("name", str("NAME"), 50, '', $_REQUEST["name"]);
+    form_label(str('USERS_NAME_PROMPT'));
+    form_list_dynamic("cert", str('USERS_MAX_CERT'), get_cert_list_sql(), $_REQUEST["cert"]);
+    form_label(str('USERS_MAX_CERT_PROMPT'));
+    form_input('pin',str('USERS_PIN'),5,10);
+    form_label(str('USERS_PIN_PROMPT'));
+    form_submit(str('USERS_ADD_BUTTON'), 2);
+    form_end();
+  }
+  
+  function users_new()
+  {
+    $name = $_REQUEST["name"];
+    $cert = $_REQUEST["cert"];
+    $pin  = $_REQUEST["pin"];
+    
+    if(empty($name))
+    {
+      users_display("", "!".str('USERS_ERROR_NAME'));
+    }
+    elseif (empty($cert))
+    {
+      users_display("","!".str('USERS_ERROR_CERT'));
+    }
+    else
+    {
+      $user_count = db_value("select count(*) from users where name='".db_escape_str($name)."'");
+      
+      if($user_count > 0)
+      {
+        users_display("", "!".str('USERS_ERROR_EXISTS'));
+      }
+      else
+      {
+        $data = array("name"=>$name, "maxcert"=>$cert,'pin'=>$pin);
+
+        if(db_insert_row("users", $data) === false)
+          users_display(db_error());
+        else
+          users_display("", str('USERS_ADDED_OK'));
+      }
+    }
+  }
+  
+  function users_modify()
+  {
+    $selected = form_select_table_vals("user_id");
+    $edit_id = form_select_table_edit("user_id", "users");
+    $update_data = form_select_table_update("user_id", "users");
+    
+    if(!empty($edit_id))
+    {
+      users_display("", "", $edit_id);
+    }
+    elseif(!empty($update_data))
+    {
+      $user_id = $update_data["USER_ID"];
+      $name = $update_data["NAME"];
+      $max_cert = $update_data["MAX_CERTIFICATE_VIEWABLE"];
+      $pin = $update_data["PIN"];
+      
+      if(empty($name))
+      {
+        user_display("!".str('USERS_ERROR_NAME'));
+      }
+      else
+      {
+        $sql = "update users set name='".db_escape_str($name)."'";
+        if(empty($pin))
+          $sql = $sql.",pin=NULL";
+        else
+          $sql = $sql.",pin='".db_escape_str($pin)."'";
+        
+        $sql = $sql.",maxcert=$max_cert where user_id=$user_id";
+        
+        db_sqlcommand($sql);
+        users_display(str('USERS_EDIT_OK'));
+      }
+    }
+    elseif(!empty($selected))
+    {
+      $message = str('USERS_DEL_OK');
+      
+      foreach($selected as $selected_item)
+      {
+        if($selected_item != 1)
+        {
+          db_sqlcommand("delete from users where user_id=$selected_item");
+        }
+        else
+          $message = "!".str('USERS_ERROR_DEFAULT');
+      }
+      
+      users_display($message);
+    }
+    else
+      users_display();
+  }
+  
+/**************************************************************************************************
+                                               End of file
+ **************************************************************************************************/
+?>
