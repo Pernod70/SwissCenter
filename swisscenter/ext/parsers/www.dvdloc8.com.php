bae5edcd080712b6bb53b13f84e70651df737a92
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2007-05-08 15:02:08 +0000

    The filename being used to store downloaded albumart on disk was being incorrectly formed using the basename() function instead of dirname().
    Thanks to Crook/Pernod for posting this fix to the forums.
    
    Fixes #5.

--- swisscenter/ext/parsers/www.dvdloc8.com.php
+++ swisscenter/ext/parsers/www.dvdloc8.com.php
@@ -8,7 +8,7 @@
     // The site URL (may be used later)
     $site_url    = 'http://www.dvdloc8.com/';
     
-    // Determine the best match for the movie and retrieve the HTML for relevant the details page.
+    // Determine the best match for the movie and retrieve the HTML for details page.
     $html = search_for_movie( $title
                             , $site_url
                             , 'fast_search_action.php?keywords=#####&searchtype=title'
@@ -24,10 +24,10 @@
       $img_addr = get_html_tag_attrib($html,'img','images/dvdcover','src');
       if ($img_addr !== false)
         file_save_albumart( add_site_to_url($img_addr, $site_url)
-                          , basename($filename).'/'.file_noext($filename).'.'.file_ext($img_addr)
+                          , dirname($filename).'/'.file_noext($filename).'.'.file_ext($img_addr)
                           , $title);
     
-      // Get moviethe single-value movie attributes 
+      // Get single-value movie attributes 
       $cert_text = get_html_tag_attrib($html,'img','images/rating','alt');
       $synopsis  = substr_between_strings($html,'Synopsis:','<br><br>');
       $year      = substr_between_strings($html,'Year of Production:','Disc Release Date');
