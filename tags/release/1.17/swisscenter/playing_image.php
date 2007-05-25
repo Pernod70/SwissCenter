81656dc19865e092f163252760455b19b26da7a5
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2007-05-25 12:15:48 +0000

    Re-release of 1.17

---
+++ tags/release/1.17/swisscenter/playing_image.php
@@ -0,0 +1,31 @@
+<?
+/**************************************************************************************************
+   SWISScenter Source                                                              Robert Taylor
+ *************************************************************************************************/
+
+  require_once( realpath(dirname(__FILE__).'/base/image_screens.php'));
+
+  $tracks    = get_tracklist();
+  $idx       = $_REQUEST["idx"];
+  $prev_info = array();
+  $next_info = array();
+
+  // If the hardware player doesn't support a "Now Playing" screen, then we try to fake one by
+  // looking in the session at the index of the last file streamed to the player.
+  if (!support_now_playing())
+    $idx = $_SESSION["LAST_RESPONSE_IDX"];
+  
+  // Get the previous and next track details if available.
+  if ($idx > 0)
+    $prev_info = $tracks[$idx-1];
+  if ($idx < count($tracks)-1)
+    $next_info = $tracks[$idx+1];
+    
+  // Generate and display the "Now Playing" screen.    
+  $image = now_playing_image( $tracks[$idx], $prev_info, $next_info, ($idx+1).' / '.count($tracks) );
+  $image->output('jpeg');
+
+/**************************************************************************************************
+                                               End of file
+ **************************************************************************************************/
+?>
