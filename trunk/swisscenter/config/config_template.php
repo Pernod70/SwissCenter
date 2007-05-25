ce63937fae55777287f1b725d4e27588464e6083
izzy <izzy@5fb36b96-46d7-3642-b0fa-166f5347550f>
2007-05-25 17:03:42 +0000

    + Updated German translations. ! Encoding was mistakenly set to UTF-8

--- trunk/swisscenter/config/config_template.php
+++ trunk/swisscenter/config/config_template.php
@@ -6,7 +6,7 @@
 
 <html>
 <head>
-  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  <meta http-equiv="content-type" content="text/html;charset=Windows-1252" />
   <title>SwissCenter Config</title>
   <link rel="stylesheet" type="text/css" media="screen" href="sdmenu.css" />
   <link rel="stylesheet" type="text/css" media="screen" href="config.css" />
