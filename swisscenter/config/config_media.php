4cbfa6686efa8e0dd2c28e5fd0a5db51a2327400
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2007-05-29 14:56:55 +0000

    You can now choose which media type and/or category to refresh in the "Search for new media" configuration page.

--- swisscenter/config/config_media.php
+++ swisscenter/config/config_media.php
@@ -7,7 +7,16 @@ require_once( realpath(dirname(__FILE__).'/../base/media.php'));
 
   function media_search()
   {
+    // Store the parameters to the media search (catgory, media tyoe) in the system_prefs table
+    // as this is the only way of passing the info to the background process in Simese.
+    set_sys_pref('MEDIA_SCAN_MEDIA_TYPE',$_REQUEST["type"]);
+    set_sys_pref('MEDIA_SCAN_CATEGORY',$_REQUEST["cat"]);
+    set_sys_pref('MEDIA_SCAN_STATUS','Pending');
+ 
+    // Call the media search in the background.    
     media_refresh_now();
+    
+    // Inform the user that it is happening.
     echo "<h1>".str('SETUP_SEARCH_NEW_MEDIA')."</h1>";
     message(str('REFRESH_DATABASE_PROMPT'));
     echo '<p>'.str('REFRESH_RUNNING');
@@ -15,12 +24,17 @@ require_once( realpath(dirname(__FILE__).'/../base/media.php'));
 
   function media_refresh()
   {  
+    echo '<h1>'.str('SETUP_SEARCH_NEW_MEDIA').'</h1>
+          <p>'.str('MEDIA_SEARCH_PROMPT').'<br>&nbsp;';
+
     form_start('index.php', 150, 'conn');
     form_hidden('section', 'MEDIA');
     form_hidden('action', 'SEARCH');
-    echo '<h1>'.str('SETUP_SEARCH_NEW_MEDIA').'</h1>
-          <p>'.str('MEDIA_SEARCH_PROMPT').'<br>&nbsp;';
-    form_submit(str('SETUP_SEARCH_NEW_MEDIA'),2,'left',240);
+    form_list_dynamic('type',str('MEDIA_TYPE'),"select media_id,media_name from media_types where media_id<>".MEDIA_TYPE_RADIO." order by 2",$_REQUEST['type'],true,false,'All Media Types');
+    echo '<tr><td></td><td> &nbsp; &nbsp; '.str('OR').'</td></tr>';
+    form_list_dynamic('cat', str('CATEGORY'),"select cat_id,cat_name from categories order by cat_name", $_REQUEST['cat'],true,false,'All Categories');
+    echo '<tr><td></td><td>&nbsp;</td></tr>';    
+    form_submit(str('SETUP_SEARCH_NEW_MEDIA'),2);
     form_end();
   }
 
--- swisscenter/media_search.php
+++ swisscenter/media_search.php
@@ -10,7 +10,7 @@
   require_once( realpath(dirname(__FILE__).'/base/media.php'));
   require_once( realpath(dirname(__FILE__).'/video_obtain_info.php'));
 
-  set_time_limit(0);
+  set_time_limit(86400);
   ini_set('memory_limit',-1);
   $start_time = time();
   $cache_dir = get_sys_pref('cache_dir');
@@ -20,18 +20,26 @@
   // records which are no longer available.
   // ----------------------------------------------------------------------------------
 
-  function process_media_dirs( $media_type )
+  function process_media_dirs( $media_type = '', $cat_id = '')
   {
-    $media_locations = db_toarray("select * from media_locations where media_type=$media_type");
-    $table           = db_value("select media_table from media_types where  media_id=$media_type");
-    $types           = media_exts( $media_type );
+    // Get a list of matching locations
+    $media_locations = db_toarray("select * 
+                                   from media_locations
+                                   where media_type != ".MEDIA_TYPE_RADIO.
+                                   (empty($cat_id) ? '' : " and cat_id = $cat_id").
+                                   (empty($media_type) ? '' : " and media_type = $media_type")
+                                 );
 
-    send_to_log(4,'Refreshing '.strtoupper($table).' database');
-      
+    // Process each location
     foreach ($media_locations as $location)
+    {
+      $table = db_value("select media_table from media_types where  media_id = $location[MEDIA_TYPE]");
+      $types = media_exts( $location["MEDIA_TYPE"] );
+      send_to_log(4,'Refreshing '.strtoupper($table).' database');
       process_media_directory( str_suffix($location["NAME"],'/'), $location["LOCATION_ID"], $table, $types );
+      send_to_log(4,'Completed refreshing '.strtoupper($table).' database');
+    }
         
-    send_to_log(4,'Completed refreshing '.strtoupper($table).' database');
   }
 
   //===========================================================================================
@@ -39,12 +47,25 @@
   //===========================================================================================
 
   media_indicator('BLINK');
-  db_sqlcommand("update media_locations set percent_scanned=0 where media_type in (".MEDIA_TYPE_MUSIC.",".MEDIA_TYPE_VIDEO.",".MEDIA_TYPE_PHOTO.")");
-  
-  process_media_dirs( MEDIA_TYPE_MUSIC );
-  process_media_dirs( MEDIA_TYPE_VIDEO );
-  process_media_dirs( MEDIA_TYPE_PHOTO );
 
+  // If there are parameters for the media search then read them and then remove them.
+  $media_type = get_sys_pref('MEDIA_SCAN_MEDIA_TYPE');
+  $cat_id     = get_sys_pref('MEDIA_SCAN_CATEGORY');
+  delete_sys_pref('MEDIA_SCAN_MEDIA_TYPE');
+  delete_sys_pref('MEDIA_SCAN_CATEGORY');
+  set_sys_pref('MEDIA_SCAN_STATUS','Started');
+  
+  // Set the percent_scanned to zero for all locations due to be scanned.
+  db_sqlcommand("update media_locations set percent_scanned=0 
+                  where media_type != ".MEDIA_TYPE_RADIO.
+                  (empty($cat_id) ? '' : " and cat_id = $cat_id").
+                  (empty($media_type) ? '' : " and media_type = $media_type")
+               );
+
+  // Scan the appropriate media directories
+  process_media_dirs( $media_type, $cat_id );
+
+  // update video details from the Internet if enabled
   if ( is_movie_check_enabled() )
     extra_get_all_movie_details();
   
@@ -52,10 +73,7 @@
   remove_orphaned_movie_info();
   eliminate_duplicates();
   media_indicator('OFF');
- 
-  // Tell the MusicIP server (if present) to refresh it's cache and validate any new songs.
-  musicip_server_refresh_cache();
-  musicip_server_validate();
+  set_sys_pref('MEDIA_SCAN_STATUS','Complete');
 
 /**************************************************************************************************
                                                End of file
