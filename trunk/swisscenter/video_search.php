e0e934e6718d1df8ec08d913746305b7bd815f8a
Pernod <Pernod@5fb36b96-46d7-3642-b0fa-166f5347550f>
2008-05-12 21:22:09 +0000

    Improved database query performance when browsing videos. Previously a video query would join all director, actor, and genre tables. Now only those relevant for the current search are joined.

--- trunk/swisscenter/video_search.php
+++ trunk/swisscenter/video_search.php
@@ -7,13 +7,8 @@
 
   $column        = $_REQUEST["sort"];
   $title         = str('BROWSE_'.strtoupper($column));
-  $joined_tables = 'left outer join directors_of_movie dom   on media.file_id   = dom.movie_id
-                    left outer join genres_of_movie gom      on media.file_id   = gom.movie_id
-                    left outer join actors_in_movie aim      on media.file_id   = aim.movie_id
-                    left outer join actors a                 on aim.actor_id    = a.actor_id
-                    left outer join directors d              on dom.director_id = d.director_id
-                    left outer join genres g                 on gom.genre_id    = g.genre_id'.
-                    get_rating_join().viewed_join(MEDIA_TYPE_VIDEO);
+  $joined_tables = get_rating_join().viewed_join(MEDIA_TYPE_VIDEO);
+  $history       = search_hist_most_recent();
   
   switch ($column)
   {
@@ -30,6 +25,17 @@
       $column = "IFNULL(media_cert.name,unrated_cert.name)";
       break;
   }
+  
+  // Only join tables that are actually required
+  if ($column == 'director_name' || strpos($history["sql"],'director_name like') > 0)
+    $joined_tables .= 'left outer join directors_of_movie dom on media.file_id = dom.movie_id '.
+                      'left outer join directors d on dom.director_id = d.director_id ';
+  if ($column == 'actor_name' || strpos($history["sql"],'actor_name like') > 0)
+    $joined_tables .= 'left outer join actors_in_movie aim on media.file_id = aim.movie_id '.
+                      'left outer join actors a on aim.actor_id = a.actor_id ';
+  if ($column == 'genre_name' || strpos($history["sql"],'genre_name like') > 0)
+    $joined_tables .= 'left outer join genres_of_movie gom on media.file_id = gom.movie_id '.
+                      'left outer join genres g on gom.genre_id = g.genre_id ';
 
   search_media_page( str('WATCH_MOVIE'), $title, MEDIA_TYPE_VIDEO, $joined_tables, $column, 'video_selected.php');
   
