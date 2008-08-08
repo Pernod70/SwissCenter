d73c27ab180871e606b24a04bd802f5cbf2a24c3
Pernod <Pernod@5fb36b96-46d7-3642-b0fa-166f5347550f>
2008-08-08 12:20:49 +0000

    Added support for the HDX 900 player, it's another NMT so is treated in the same way as the Popcorn Hour.

--- trunk/swisscenter/base/capabilities.php
+++ trunk/swisscenter/base/capabilities.php
@@ -55,6 +55,8 @@ function get_player_type()
       $type = 'POPCORN';
     elseif ( strpos($_SERVER['HTTP_USER_AGENT'],'-QPG-')!== false )
       $type = 'POPCORN';
+    elseif ( strpos($_SERVER['HTTP_USER_AGENT'],'-HDD-')!== false )
+      $type = 'POPCORN';
     elseif ( strpos($_SERVER['HTTP_USER_AGENT'],'MSIE')!== false )
       $type = 'PC';
     elseif ( strpos($_SERVER['HTTP_USER_AGENT'],'Mozilla')!== false )
