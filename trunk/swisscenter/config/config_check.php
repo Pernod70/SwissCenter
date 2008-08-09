a3f395dafe2243080217ff86d2b74b9799f5a6ab
Pernod <Pernod@5fb36b96-46d7-3642-b0fa-166f5347550f>
2008-08-09 17:49:32 +0000

    Added Apache version to the Webserver check in System Tests.

--- trunk/swisscenter/config/config_check.php
+++ trunk/swisscenter/config/config_check.php
@@ -14,22 +14,22 @@ class test_summary
 {
   var $title;
   var $sections;
-  
+
   var $pass_img = 'pass.png';
   var $fail_img = 'fail.png';
 
   function test_summary( $title )
-  {    
+  {
     $this->sections = array();
     $this->title    = $title;
   }
 
   function set_pass_icon( $icon )
   { $this->pass_img = $icon; }
-  
+
   function set_fail_icon( $icon )
   { $this->fail_img = $icon; }
-  
+
   #-------------------------------------------------------------------------------------------------
   # Add a new section and returns the identifier for it (used when adding tests).
   #-------------------------------------------------------------------------------------------------
@@ -41,8 +41,8 @@ class test_summary
                                       , "failed"  => array()  );
     return $position;
   }
-  
-  
+
+
   #-------------------------------------------------------------------------------------------------
   # Add details about a specific test
   #-------------------------------------------------------------------------------------------------
@@ -56,10 +56,10 @@ class test_summary
       else
         $this->sections[$section]["failed"][] = $fail;
     }
-    else 
+    else
       echo '<li>'.$section.' - '.$key;
   }
-  
+
   #-------------------------------------------------------------------------------------------------
   # Display the results
   #-------------------------------------------------------------------------------------------------
@@ -67,24 +67,24 @@ class test_summary
   function display()
   {
     ksort($this->sections);
-    
+
     echo '<h1>'.$this->title.'</h1>
           <p><center>';
     foreach ($this->sections as $section)
     {
       $failures = (count($section["failed"]) > 0);
-      
+
       // Show the heading for this section (complete with pass/fail icon)
       echo '<p><table border=1 noshade width="90%" cellspacing=0 cellpadding=8 bgcolor="#ffffff">
             <tr>
               <td width="100%"><b>'.$section["heading"].'</b></td>
               <td width="40" valign=top align=center><img src="/images/'.($failures ? $this->fail_img : $this->pass_img).'"></td>
             </tr>';
-      
+
       if ( $failures )
-      {      
+      {
         echo '<tr><td colspan=2>';
-        
+
         if (count($section["passed"]) > 0)
         {
           echo '<p>'.str('INSTALL_TEST_SUCCESS').'<font color="#006600"><ul>';
@@ -92,7 +92,7 @@ class test_summary
             echo '<li>'.$msg;
           echo '</ul></font>';
         }
-        
+
         if (count($section["failed"]) > 0)
         {
           echo '<p>'.str('INSTALL_TEST_FAIL_WEB').'<font color="#660000"><ul>';
@@ -100,7 +100,7 @@ class test_summary
             echo '<li>'.$msg;
           echo '</ul></font>';
         }
-  
+
         echo '</td>';
       }
 
@@ -122,11 +122,11 @@ function check_display()
   $core_tests      = new test_summary( str('INSTALL_TEST_TITLE') );
   $component_tests = new test_summary( str('EXTRAS_TEST_TITLE') );
   $component_tests->set_fail_icon('question.png');
-  
+
   # ----------------------
   # SwissCenter configuration Tests
   # ----------------------
-                           
+
   $swiss = $core_tests->add_section("SwissCenter : v".swisscenter_version(),4);
 
   // It only makes sense to check for root installations on UNIX.
@@ -138,13 +138,13 @@ function check_display()
   $core_tests->add_test( $swiss, check_swiss_write_cache_dir(), str("PASS_SWISS_CACHE"), str("FAIL_SWISS_CACHE", get_sys_pref('cache_dir')) );
   $core_tests->add_test( $swiss, check_swiss_write_playlist_dir(), str("PASS_SWISS_PLAYLIST"), str("FAIL_SWISS_PLAYLIST", get_sys_pref('playlists')) );
   $core_tests->add_test( $swiss, check_swiss_write_log_dir(), str("PASS_SWISS_LOG"), str("FAIL_SWISS_LOG", logfile_location()) );
-                      
+
   # ----------------------
   # PHP Tests
   # ----------------------
-  
+
   $php = $core_tests->add_section("PHP : v".phpversion(),2);
-  
+
   $core_tests->add_test( $php, check_php_version(), str("PASS_PHP_VERSION"), str("FAIL_PHP_VERSION",phpversion()) );
   $core_tests->add_test( $php, check_php_required_modules(), str("PASS_PHP_REQ_MODS"), str("FAIL_PHP_REQ_MODS", implode(', ',get_required_modules_list())) );
   $core_tests->add_test( $php, check_php_suggested_modules(), str("PASS_PHP_EXTRA_MODS"), str("FAIL_PHP_EXTRA_MODS", implode(', ',get_suggested_modules_list())) );
@@ -153,24 +153,24 @@ function check_display()
   # ----------------------
   # MySQL Tests
   # ----------------------
-  
+
   # If there is no swisscenter.ini file present, then we will not be able to connect to the database
   # because that is where the connection details are stored.
 
   if ( check_swiss_ini_file() )
-  {                         
+  {
     $mysql = $core_tests->add_section("MySQL : v".mysql_version(),3);
-  
-    $core_tests->add_test( $mysql, check_mysql_connect(), str("PASS_MYSQL_CONNECT"), str("FAIL_MYSQL_CONNECT"));  
+
+    $core_tests->add_test( $mysql, check_mysql_connect(), str("PASS_MYSQL_CONNECT"), str("FAIL_MYSQL_CONNECT"));
     $core_tests->add_test( $mysql, check_mysql_version(), str("PASS_MYSQL_VERSION"), str("FAIL_MYSQL_VERSION"));
     $core_tests->add_test( $mysql, check_mysql_database_exists(), str("PASS_MYSQL_DB"), str("FAIL_MYSQL_DB"));
   }
-                      
+
   # ----------------------
   # Webserver Tests
   # ----------------------
 
-  $server = $core_tests->add_section("Webserver ".(is_server_simese() ? ": Simese v".simese_version() : ""),1);
+  $server = $core_tests->add_section("Webserver : ".(is_server_simese() ? "Simese v".simese_version() : "Apache v".apache_version()),1);
   $core_tests->add_test( $server, check_server_scheduler(), str("PASS_SERVER_SCHED"), str("FAIL_SERVER_SCHED"));
 
   # ----------------------
@@ -178,7 +178,7 @@ function check_display()
   # ----------------------
 
   $musicip = $component_tests->add_section("MusicIP",1);
-  
+
   $component_tests->add_test( $musicip, musicip_available(), str('PASS_MUSICIP_TEST'),str('FAIL_MUSICIP_TEST').'<p>'.str('MIP_DESC','<a href="http://www.musicip.com">www.musicip.com</a>'),FALSE);
 
   if ( musicip_available() )
