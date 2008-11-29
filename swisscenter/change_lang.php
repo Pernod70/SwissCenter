4a60eacbd97b89e057883e16ae49baaf34e400ad
Nigel Barnes <1696956+Pernod70@users.noreply.github.com>
2008-11-29 23:40:18 +0000

    Added paging to the language selection page.

--- swisscenter/change_lang.php
+++ swisscenter/change_lang.php
@@ -3,29 +3,27 @@
    SWISScenter Source                                                              Robert Taylor
  *************************************************************************************************/
   require_once( realpath(dirname(__FILE__).'/base/page.php'));
-  require_once( realpath(dirname(__FILE__).'/base/menu.php'));
-  require_once( realpath(dirname(__FILE__).'/base/mysql.php'));
+  require_once( realpath(dirname(__FILE__).'/base/browse.php'));
   require_once( realpath(dirname(__FILE__).'/base/language.php'));
 
   if (isset($_REQUEST["lang"]))
   {
-  	set_user_pref('LANGUAGE',$_REQUEST["lang"]);
-  	load_lang($_REQUEST["lang"]);
-  	page_inform(2,'index.php',str('LANG_CHANGE'),str('SAVE_SETTINGS_OK'));
+    set_user_pref('LANGUAGE',$_REQUEST["lang"]);
+    load_lang($_REQUEST["lang"]);
+    page_inform(2,'index.php',str('LANG_CHANGE'),str('SAVE_SETTINGS_OK'));
   }
   else 
   {
-    page_header( str('LANG_CHANGE'), '');
-
-    $menu = new menu();
-
+    $page = isset($_REQUEST["page"]) ? $_REQUEST["page"] : 0;
+    $array = array();
     foreach (explode("\n",str_replace("\r",null,file_get_contents(SC_LOCATION.'lang/languages.txt'))) as $line)
     {
       $lang = explode(',',$line);
-      $menu->add_item($lang[0],'change_lang.php?lang='.$lang[1]);
+      $array[] = array("name"=>$lang[0], "url"=>$lang[1]);
     }
 
-    $menu->display();
+    page_header( str('LANG_CHANGE'), '');
+    browse_array(url_remove_param(current_url(),'page'), $array, $page);
     page_footer( 'config.php' );
   }
   
