4e55be6b13347de7482d81df2ab16531b7b554d5
swiss <swiss@5fb36b96-46d7-3642-b0fa-166f5347550f>
2009-02-10 18:54:34 +0000

    Added Klaasvaak's changes to display the previous two tracks on the Radio "Now Playing" screen.

--- trunk/swisscenter/music_radio_image.php
+++ trunk/swisscenter/music_radio_image.php
@@ -15,41 +15,50 @@
    */
   function shoutcast_now_playing($host, $port)
   {
+    $info = "";
+    $songs = array();
+
+    // Retrieve the HTML page detailing the recently played songs from the server
+
     send_to_log(5,'Connecting to ShoutCast server '.$host.':'.$port);
-    $fp = @fsockopen($host,$port,$errno,$errstr,10);
-    if (!$fp) 
-    { 
-      send_to_log(5,'- Connection refused'); // Displays when sever is offline
-    } 
-    else
-    { 
-      fputs($fp, "GET /7.html HTTP/1.0\r\nUser-Agent: Mozilla\r\n\r\n");
-      while (!feof($fp)) 
-      {
-        $info = fgets($fp);
-      }
-      $info = str_replace('</body></html>', "", $info);
-      $split = explode(',', $info);
-      if (empty($split[6]) )
-      {
-        send_to_log(5,'- The current song is not available'); // Displays when server is online but no song title
-      }
-      else
-      {
-        $title = str_replace('\'', '`', $split[6]);
-        $title = str_replace(',', ' ', $title);
-        send_to_log(5,'- Now playing "'.$title.'"'); 
-        return $title; // Displays song
-      }
+    if (($fp = @fsockopen($host,$port,$errno,$errstr,10)) === FALSE)
+    {
+      send_to_log(2,'- Connection refused to ShoutCast server',array("Host"=>$host,"Port"=>$port));
+      return false;
     }
-    return false;
+    else
+    {
+      fputs($fp, "GET /played.html HTTP/1.0\r\nUser-Agent: Mozilla\r\n\r\n");
+      while (!feof($fp))
+        $info .= fgets($fp);
+    }
+
+    // Process the results into an array
+
+    $info = explode('<td>',$info);
+    if (count($info)<6)
+    {
+      send_to_log(5,'- The current song is not available');
+      return false;
+    }
+    else
+    {
+      for ($i=(count($info)-6)/2; $i>0; $i--)
+        array_unshift($songs, strip_tags($info[$i*2+6]));
+    }
+
+    // Return the results
+
+    send_to_log(8,'Songs returned from the Shoutcast server',$songs);
+    return $songs[0].'<>'.$songs[1].'<>'.$songs[2];
   }
+
 
   if ( strpos($_REQUEST["playlist"],'http')===0 && (empty($_REQUEST["host"]) || empty($_REQUEST["port"])) )
   {
     // Retrieve server details from playlist (pls) (3 attempts)
     $i=0;
-    send_to_log(6,'Attempting to obtain server details from radio playlist'); 
+    send_to_log(6,'Attempting to obtain server details from radio playlist');
     while ( $i < 3 && empty($info) )
     {
       if ( ($info = file_get_contents(un_magic_quote($_REQUEST["playlist"]))) == false )
@@ -66,7 +75,7 @@
     $host[1] = $_REQUEST["host"];
     $host[2] = $_REQUEST["port"];
   }
-  
+
   if ( isset($_REQUEST["list"]))
   {
     // List of images to display to the user (changes every 30 seconds)
@@ -86,7 +95,7 @@
   {
     // Get now playing details
     $playing = shoutcast_now_playing($host[1], $host[2]);
-    
+
     // Generate and display the "Now Playing" screen.
     // - If EVA700 then only send a new image if the details have changed. Avoids continuous refreshing.
     if (get_player_type()!=='NETGEAR' || $_SESSION["now_playing"]!==$playing )
@@ -108,6 +117,7 @@
 
       $image = station_playing_image($station, $playing, $logo);
       $image->output('jpeg');
+
     }
   }
 
