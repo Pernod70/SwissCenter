3c583421d0a921a9d96fa15109843332b44bced0
Nigel Barnes <1696956+Pernod70@users.noreply.github.com>
2009-02-28 21:57:10 +0000

    Updated the search media page to use the PAGE_KEYBOARD background.

--- swisscenter/base/search.php
+++ swisscenter/base/search.php
@@ -10,7 +10,7 @@
   require_once( realpath(dirname(__FILE__).'/rating.php'));
   require_once( realpath(dirname(__FILE__).'/media.php'));
   require_once( realpath(dirname(__FILE__).'/filter.php'));
-  
+
 #-------------------------------------------------------------------------------------------------
 # Functions for managing the search history.
 #-------------------------------------------------------------------------------------------------
@@ -19,7 +19,7 @@ function search_hist_init( $url = '', $sql = '')
 {
   $_SESSION["picker"]  = array();
   $_SESSION["history"] = array();
-  
+
   if (!empty($url))
     $_SESSION["history"][] = array("url"=> $url, "sql"=>$sql);
 }
@@ -39,7 +39,7 @@ function search_picker_pop()
   if (count($_SESSION["picker"]) == 0)
     page_error(str('DATABASE_ERROR'));
 
-  return array_pop($_SESSION["picker"]);  
+  return array_pop($_SESSION["picker"]);
 }
 
 function search_picker_most_recent()
@@ -47,7 +47,7 @@ function search_picker_most_recent()
   if (count($_SESSION["picker"]) == 0)
     page_error(str('DATABASE_ERROR'));
 
-  return $_SESSION["picker"][count($_SESSION["picker"])-1]; 
+  return $_SESSION["picker"][count($_SESSION["picker"])-1];
 }
 
 function search_hist_push( $url, $sql )
@@ -67,7 +67,7 @@ function search_hist_most_recent()
 {
   if (count($_SESSION["history"]) == 0)
     page_error(str('DATABASE_ERROR'));
-  
+
   return $_SESSION["history"][count($_SESSION["history"])-1];
 }
 
@@ -81,13 +81,13 @@ function search_hist_first()
 
 #-------------------------------------------------------------------------------------------------
 # Function to output a "search" page for any media type.
-#-------------------------------------------------------------------------------------------------  
+#-------------------------------------------------------------------------------------------------
 
 function  search_media_page( $heading, $title, $media_type, $joined_tables, $column,  $choose_url )
 {
   // Make sure that the session variable for "shuffle" matches the user's preference (because it will have been set "on" for quick play).
   $_SESSION["shuffle"] = get_user_pref('shuffle','off');
-  
+
   // Should we delete the last entry on the history stack?
   if (isset($_REQUEST["del"]) && strtoupper($_REQUEST["del"]) == 'Y')
     search_hist_pop();
@@ -107,7 +107,7 @@ function  search_media_page( $heading, $title, $media_type, $joined_tables, $col
   $main_table     = get_media_table($media_type);
   $main_table_sql = "$main_table media ";
   $restrict_sql   = "$column like '$prefix".db_escape_str(str_replace('_','\_',$search))."%' $history[sql]";
-  
+
   $viewed_sql     = "select concat( sum(if(v.total_viewings>0,1,0)),':',count(*) ) view_status
                      from $main_table_sql $joined_tables";
 
@@ -115,7 +115,7 @@ function  search_media_page( $heading, $title, $media_type, $joined_tables, $col
   $choose_url = url_set_params($choose_url, array('add'=>'Y', 'type'=>$column));
 
   // Get the matching records from the database.
-  $data       = db_toarray("   select $column display 
+  $data       = db_toarray("   select $column display
                                  from $main_table_sql $joined_tables
                                 where $column != '0' and $restrict_sql and ml.media_type=$media_type
                              group by $column
@@ -126,23 +126,23 @@ function  search_media_page( $heading, $title, $media_type, $joined_tables, $col
 
   if ( $data === false || $num_rows === false)
     page_error(str('DATABASE_ERROR'));
-    
+
   if ($prefix == '')
-    $valid = strtoupper(join(db_col_to_list(" select distinct upper(substring($column,".(strlen($search)+1).",1)) display 
-                                                from $main_table_sql $joined_tables 
-                                               where $column !='0' and $restrict_sql and ml.media_type=$media_type 
+    $valid = strtoupper(join(db_col_to_list(" select distinct upper(substring($column,".(strlen($search)+1).",1)) display
+                                                from $main_table_sql $joined_tables
+                                               where $column !='0' and $restrict_sql and ml.media_type=$media_type
                                             group by $column
                                               having ".viewed_status_predicate( filter_get_name() )."
                                             order by 1")));
   else
     $valid = '';
 
-  // Remove last picker state before adding the new one 
+  // Remove last picker state before adding the new one
   if (isset($_REQUEST["p_del"]) && strtoupper($_REQUEST["p_del"]) == 'Y')
     search_picker_pop();
-    
+
   // Start outputting the page
-  page_header( $heading, $title.' : '.$search, '', $focus );
+  page_header( $heading, $title.' : '.$search, '', $focus, false,'','PAGE_KEYBOARD');
   echo '<table border=0 width="100%"><tr><td width="'.convert_x(300).'" valign="top">';
   show_picker( url_set_param($this_url,'page',0), $search, '', $valid);
   echo '</td><td valign=top>';
@@ -188,7 +188,7 @@ function  search_media_page( $heading, $title, $media_type, $joined_tables, $col
 
   page_footer( url_add_param($history["url"],'p_del','Y'), $buttons);
 }
-  
+
 #-------------------------------------------------------------------------------------------------
 # Function to process the paramters passed by the function above when an item has been selected.
 #-------------------------------------------------------------------------------------------------
@@ -198,11 +198,11 @@ function search_process_passed_params()
   $name      = un_magic_quote(rawurldecode($_REQUEST["name"]));
   $type      = un_magic_quote($_REQUEST["type"]);
   $history   = search_hist_most_recent();
-  
+
   // If no $type is specified, then $name contains a pure SQL predicate to add.
   if (empty($type))
     $predicate = $history["sql"]." and $name";
-  else 
+  else
     $predicate = $history["sql"]." and $type like '".db_escape_str(str_replace('_','\_',$name))."'";
 
   if (isset($_REQUEST["shuffle"]))
@@ -217,12 +217,12 @@ function search_process_passed_params()
     $hist_url  = url_remove_params(current_url(), array('add','p_del'));
     search_hist_push( $hist_url, $predicate );
   }
-  
+
   if (isset($_REQUEST["p_del"]) && strtoupper($_REQUEST["p_del"]) == 'Y')
   {
     search_picker_pop();
   }
-  
+
   return $predicate;
 }
 
@@ -250,7 +250,7 @@ function search_distinct_info (&$info, $info_text, $column, $table, $predicate)
     $info->add_item($info_text, $col);
     return $col;
   }
-  else 
+  else
     return '';
 }
 
