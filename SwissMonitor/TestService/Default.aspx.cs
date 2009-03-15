abd8c6780e3b689613f8c9dd17c093819e0fede1
garfoot <garfoot@5fb36b96-46d7-3642-b0fa-166f5347550f>
2009-03-15 22:42:47 +0000

    Now checks for an XML response from SwissCenter when notifying it of a change and supports the notifier aborting retries. Closes #183, refs #184.

--- SwissMonitor/TestService/Default.aspx.cs
+++ SwissMonitor/TestService/Default.aspx.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Xml;
 
 namespace TestService
 {
@@ -6,11 +10,47 @@ namespace TestService
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            const string swissNamespace = "http://www.swisscenter.co.uk/schemas/2009/03/SwissMonitor";
+
+            
+            //StringBuilder output = new StringBuilder();
+            XmlWriterSettings writerSettings = new XmlWriterSettings {Encoding = Encoding.UTF8, Indent = true};
+
+            
+            using (MemoryStream output = new MemoryStream())
+            using (XmlWriter result = XmlWriter.Create(output, writerSettings))
+            {
+                result.WriteStartDocument();
+                result.WriteStartElement("result", swissNamespace);
+                result.WriteStartElement("status", swissNamespace);
+                result.WriteValue(ConfigurationManager.AppSettings["Status"]);
+                result.WriteEndElement();
+                result.WriteStartElement("message", swissNamespace);
+                result.WriteValue(GetData());
+                result.WriteEndElement();
+                result.WriteStartElement("retry", swissNamespace);
+                result.WriteValue(ConfigurationManager.AppSettings["retry"]);
+                result.WriteEndElement();
+                result.WriteEndElement();
+
+                result.Flush();
+
+                Response.Write(Encoding.UTF8.GetString(output.GetBuffer()));
+            }
+        }
+
+        private string GetData()
+        {
+            string data = String.Empty;
             foreach(string key in Request.Form.AllKeys)
             {
-                if(!string.IsNullOrEmpty(key))
-                    Response.Write(string.Format("{0} = {1}, ", key, Request.Form[key]));
+                if (!string.IsNullOrEmpty(key))
+                {
+                    data += string.Format(string.Format("{0} = {1}, ", key, Request.Form[key]));
+                }
             }
+
+            return data;
         }
     }
 }
