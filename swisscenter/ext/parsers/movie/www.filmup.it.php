86e2d96645d25eec3a7bf243f11c5e0c43860368
Nigel Barnes <1696956+Pernod70@users.noreply.github.com>
2009-03-15 21:46:55 +0000

    Updated the filmup.it parser to use the Google API and added rating.

--- swisscenter/ext/parsers/movie/www.filmup.it.php
+++ swisscenter/ext/parsers/movie/www.filmup.it.php
@@ -1,19 +1,14 @@
 <?php
 /**************************************************************************************************
    SWISScenter Source
-   
+
    This is one of a selection of scripts all designed to obtain information from the internet
    relating to movies that the user has added to their database. It typically collects
    information such as genre, year of release, synopsis, directors and actors.
-      
-   NOTE: This parser for FilmUP.com is _NOT_ an official part of SWISScenter, and is not supported by the
-   SWISScenter developers. Nigel Barnes(Pernod)
-   
-   Version history:
-   27-Jan-2008: v1.0:     First public release.
-   01-Feb-2008: v1.1:     Improved accuracy of search results.
 
  *************************************************************************************************/
+
+require_once( SC_LOCATION."/ext/json/json.php");
 
   /**
    * Searches the filmup.it site for movie details
@@ -26,50 +21,36 @@
   function extra_get_movie_details($id, $filename, $title)
   {
     // The site URL (may be used later)
-    $site_url     = 'http://filmup.leonardo.it/';
-    $search_url   = 'http://www.google.it/search?q=+Scheda%3A+#####+site%3Afilmup.leonardo.it';
-    $search_title = str_replace('%20','+',urlencode($title));
-    
-    send_to_log(4,"Searching for details about ".$title." online at '$site_url'");
-      
-    // Get page from filmup.it using Google search
-    $url_load = str_replace('#####',$search_title,$search_url);
-    send_to_log(6,'Fetching information from: '.$url_load);
-    $html     = file_get_contents( $url_load );
+    $site_url = 'http://filmup.leonardo.it/';
     $accuracy = 0;
-    
-    if ($html === false)
+
+    // Get search results from google.
+    send_to_log(4,"Searching for details about ".$title." online at '$site_url'");
+    $results = google_api_search("Scheda: ".$title,"filmup.leonardo.it");
+
+    if (count($results)==0)
     {
-      send_to_log(2,'Failed to access the URL.');
+      send_to_log(4,"No Match found.");
+      $html = false;
     }
     else
     {
-      // Is the text that signifies a successful search present within the HTML?    
-      if (strpos(strtolower($html),strtolower('Risultati')) !== false)
-      {
-        $matches = get_urls_from_html($html, '');
-        $index   = best_match('FilmUP Scheda '.$title, $matches[2], $accuracy);
-        
-        if ($index === false)
-          $html = false;          
-        else
-        {
-          $filmup_url = add_site_to_url($matches[1][$index],$site_url);
-          send_to_log(6,'Fetching information from: '.$filmup_url);
-          $html = file_get_contents( $filmup_url );
-        }
-      }
+      $best_match = google_best_match('FilmUP - Scheda: '.$title, $results, $accuracy);
+
+      if ($best_match === false)
+        $html = false;
       else
       {
-        send_to_log(4,"No Match found.");
-        $html = false;
+        $filmup_url = $best_match->url;
+        send_to_log(6,'Fetching information from: '.$filmup_url);
+        $html = file_get_contents( $filmup_url );
       }
-    }  
-      
+    }
+
     if ($html != false)
     {
-      send_to_log(4,"Found details for '".substr_between_strings($html,'<title>','</title>')."'");  
-      
+      send_to_log(4,"Found details for '".substr_between_strings($html,'<title>','</title>')."'");
+
       // Determine the URL of the albumart and attempt to download it.
       if ( file_albumart($filename, false) == '')
       {
@@ -79,35 +60,44 @@
                             , dirname($filename).'/'.file_noext($filename).'.'.file_ext($img_addr)
                             , $title);
       }
-           
-      // Find and set multi-value movie attributes (genres, actors and directors)        
-      scdb_add_directors ( $id, explode(",", substr_between_strings($html,'Regia:&nbsp;','Sito ufficiale:')) );
-      scdb_add_actors    ( $id, explode(",", substr_between_strings($html,'Cast:&nbsp;','Produzione:')) );
-      scdb_add_genres    ( $id, explode(",", substr_between_strings($html,'Genere:&nbsp;','Durata:')) );
-        
-      // Get single-value movie attributes 
-      $synopsis = substr_between_strings($html,'Trama:<br>','<br>'); //'</f');
-      $year     = substr_between_strings($html,'Anno:&nbsp;','Genere:');
+
+      // Find and set multi-value movie attributes (genres, actors and directors)
+      scdb_add_directors ( $id, explode(",", html_entity_decode(substr_between_strings($html,'Regia:&nbsp;</font>','</font>'), ENT_QUOTES)) );
+      scdb_add_actors    ( $id, explode(",", html_entity_decode(substr_between_strings($html,'Cast:&nbsp;</font>','</font>'), ENT_QUOTES)) );
+      scdb_add_genres    ( $id, explode(",", html_entity_decode(substr_between_strings($html,'Genere:&nbsp;</font>','</font>'), ENT_QUOTES)) );
+
+      // Get single-value movie attributes
+      $synopsis = html_entity_decode(substr_between_strings($html,'Trama:<br>','<br>'), ENT_QUOTES);
+      $year     = substr_between_strings($html,'Anno:&nbsp;</font>','</font>');
+
+      // Get link for rating
+      $opinioni_uid = preg_get('/opinioni\/op.php\?uid=(\d+)"/', $html);
+      if (!empty($opinioni_uid))
+      {
+        $html = file_get_contents( $site_url.'opinioni/op.php?uid='.$opinioni_uid );
+        $user_rating = preg_get('/Media Voto:.*<b>(.*)<\/b>/Uism', $html);
+      }
 
       // Store the single-value movie attributes in the database
       $columns = array ( "YEAR"              => $year
+                       , "EXTERNAL_RATING_PC"=> (empty($user_rating) ? '' : $user_rating * 10 )
                        , "MATCH_PC"          => $accuracy
                        , "DETAILS_AVAILABLE" => 'Y'
                        , "SYNOPSIS"          => $synopsis);
       scdb_set_movie_attribs ($id, $columns);
-      return true;  
+      return true;
     }
     else
     {
       send_to_log(2,'Failed to access the URL.');
     }
-    
+
     // Mark the file as attempted to get details, but none available
     $columns = array ( "MATCH_PC" => $accuracy, "DETAILS_AVAILABLE" => 'N');
     scdb_set_movie_attribs ($id, $columns);
     return false;
   }
-  
+
 /**************************************************************************************************
                                                End of file
  **************************************************************************************************/
