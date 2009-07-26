b30ee14231421c85e121101b372038c79ba52052
Nigel Barnes <1696956+Pernod70@users.noreply.github.com>
2009-07-26 22:45:26 +0000

    Updated System Tests to check for new minimum PHP and MySQL requirements.
    Minimum PHP 5.2.6 and MySQL 5.0.

--- swisscenter/base/install_checks.php
+++ swisscenter/base/install_checks.php
@@ -7,14 +7,14 @@
 
    Checks still to implement
    -------------------------
-   
+
    * SwissCenter configuration
      * The swisscenter files are owned by the webserver user.
      * The swisscenter files (and dirs) are R/W by the webserver user.
-     
+
    * Scheduler
      * Linux   : cron is enabled for the webserver user
-     
+
   *************************************************************************************************/
 
 require_once( realpath(dirname(__FILE__).'/settings.php'));
@@ -31,7 +31,7 @@ require_once( realpath(dirname(__FILE__).'/../ext/iradio/live-radio.php'));
 function check_php_version()
 {
   send_to_log(5,'- PHP Version : '.phpversion());
-  return ( version_compare(phpversion(),'4.3.9','>=') );
+  return ( version_compare(phpversion(),'5.2.6','>=') );
 }
 
 function check_php_required_modules()
@@ -42,7 +42,7 @@ function check_php_required_modules()
       send_to_log(5,"- Required PHP module '$module' not installed");
       return false;
     }
-      
+
   return true;
 }
 
@@ -54,7 +54,7 @@ function check_php_suggested_modules()
       send_to_log(5,"- Suggested PHP module '$module' not installed");
       return false;
     }
-      
+
   return true;
 }
 
@@ -77,13 +77,13 @@ function check_php_ttf()
     $defaults[] = "/usr/share/fonts/truetype/msttcorefonts/Arial.ttf";
     $defaults[] = 'luxisr';
   }
-	
+
   // Test each default in turn.
-  foreach ($defaults as $font) 
+  foreach ($defaults as $font)
   {
     if ($img->text('Test',0,0,0,14,$font) !== false )
-    {      
-      // Font can be used! 
+    {
+      // Font can be used!
       set_sys_pref('TTF_FONT',db_escape_str($font));
       return true;
     }
@@ -108,25 +108,25 @@ function check_mysql_connect()
 
   if ( ($db = @mysql_pconnect( DB_HOST, DB_USERNAME, DB_PASSWORD )) )
     return true;
-  else 
+  else
     return false;
 }
 
 function check_mysql_version()
 {
   if ( ($db = @mysql_pconnect( DB_HOST, DB_USERNAME, DB_PASSWORD )) )
-  {  
+  {
     $stmt = mysql_query( 'select version()', $db);
     if ($row = mysql_fetch_array( $stmt, MYSQL_ASSOC ))
     {
       $version = array_pop($row);
       send_to_log(5,"- MySQL Version : $version");
-      return version_compare($version,'4.0','>=');  
+      return version_compare($version,'5.0','>=');
     }
-    else 
+    else
       return false;
-  }  
-  else 
+  }
+  else
     return false;
 }
 
@@ -135,16 +135,16 @@ function check_mysql_database_exists()
   if (check_mysql_connect())
   {
     send_to_log(5,"- Successfully connected to MySQL");
-    $result = (test_db(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_DATABASE) == 'OK'); 
-    
-    if ($result)    
+    $result = (test_db(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_DATABASE) == 'OK');
+
+    if ($result)
       send_to_log(5,"- Successfully connected to the SwissCenter database.");
     else
       send_to_log(5,"- Failed to connect to the SwissCenter database.");
 
     return $result;
   }
-  else 
+  else
   {
     send_to_log(5,"- Unable to connect to MySQL");
     return false;
@@ -170,10 +170,10 @@ function check_web_version()
     send_to_log(5,'- Apache version : '.apache_version());
     return version_compare(apache_version(),'2.0','>=');
   }
-  else 
+  else
   {
     send_to_log(5,'- Unknown webserver');
-    return true; 
+    return true;
   }
 }
 
@@ -185,7 +185,7 @@ function check_swiss_unviewable()
 {
   $max_rank = db_toarray("select max(rank) from users u join certificates c on (u.maxcert = c.cert_id)");
   $unviewable = 0;
-  
+
   foreach (db_toarray("select media_table from media_types") as $table)
   {
     $unviewable += db_value("select count(*) from $table m
@@ -194,37 +194,37 @@ function check_swiss_unviewable()
                                join certificates mlc on (mlc.cert_id = ml.unrated)
                              where ifnull(c.rank,mlc.rank) > $max_rank ");
   }
-  
+
   return $unviewable;
 }
 
 function check_swiss_write_log_dir()
 {
-  return is_writeable(dirname(logfile_location()));  
+  return is_writeable(dirname(logfile_location()));
 }
 
 function check_swiss_write_cache_dir()
 {
-  return is_writeable(get_sys_pref('cache_dir'));  
+  return is_writeable(get_sys_pref('cache_dir'));
 }
 
 function check_swiss_write_playlist_dir()
 {
-  $result =  ( is_readable(get_sys_pref('playlists')) && is_writable(get_sys_pref('playlists')) );  
+  $result =  ( is_readable(get_sys_pref('playlists')) && is_writable(get_sys_pref('playlists')) );
 
   if (!$result)
     send_to_log(5,'- Unable to access playlist directory ('.get_sys_pref('playlists').').');
-      
+
   return $result;
 }
 
 function check_swiss_ini_file()
 {
   $result =  file_exists(SC_LOCATION.'/config/swisscenter.ini');
-  
+
   if (!$result)
     send_to_log(5,'- Unable to access '.SC_LOCATION.'/config/swisscenter.ini');
-      
+
   return $result;
 }
 
@@ -234,17 +234,17 @@ function check_swiss_media_locations()
 
   if (!$result)
     send_to_log(5,'- No media locations have been defined yet.');
-        
+
   return $result;
 }
 
 function check_swiss_write_rootdir()
 {
-  $result =  ( is_readable(SC_LOCATION) && is_writable(SC_LOCATION) );  
+  $result =  ( is_readable(SC_LOCATION) && is_writable(SC_LOCATION) );
 
   if (!$result)
     send_to_log(5,'- The '.SC_LOCATION.' directory is not read/write for the webserver user.');
-      
+
   return $result;
 }
 
@@ -253,10 +253,10 @@ function check_not_root_install()
   $info = stat(SC_LOCATION.'/index.php');
   send_to_log(8,'- File Stat of /index.php',$info);
   $result = ! (is_unix() && ($info[4]==0 || $info[5]==0));
-  
+
   if (!$result)
     send_to_log(5,"- Swisscenter appears to have been installed as the 'root' user.");
-  
+
   return $result;
 }
 
@@ -273,7 +273,7 @@ function check_server_scheduler()
     {
       if (version_compare(simese_version(),'1.31','>='))
         return true;
-      else 
+      else
         return false;
     }
     else
@@ -281,7 +281,7 @@ function check_server_scheduler()
       return is_task_scheduler_running();
     }
   }
-  else 
+  else
   {
     // Linux - So check that crontab is available for use.
     $crontab = syscall("crontab -l 2>&1");
