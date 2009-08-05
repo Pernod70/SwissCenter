1d7b985408254f2d148cd1276e5569e72273837d
Pernod <Pernod@5fb36b96-46d7-3642-b0fa-166f5347550f>
2009-08-05 00:38:44 +0000

    Added option to view artist, album, track info (retrieved from Last.fm) when selecting music.

--- trunk/swisscenter/music_selected.php
+++ trunk/swisscenter/music_selected.php
@@ -10,11 +10,11 @@
   require_once( realpath(dirname(__FILE__).'/base/rating.php'));
   require_once( realpath(dirname(__FILE__).'/base/search.php'));
   require_once( realpath(dirname(__FILE__).'/base/musicip.php'));
-  
+
   //*************************************************************************************************
   // Build page elements
   //*************************************************************************************************
-  
+
   $menu       = new menu();
   $info       = new infotab();
   $sql_table  = 'mp3s media'.get_rating_join().' where 1=1 ';
@@ -23,33 +23,33 @@
   $num_rows   = db_value("select count(*) from $sql_table $predicate");
   $refine_url = 'music_search.php';
   $this_url   = url_set_param(current_url(),'add','N');
+  $meta       = '<meta SYABAS-PLAYERMODE="music">';
 
   // Output Title
   if ($num_rows ==1)
-    page_header( str('ONE_TRACK'), '');
+    page_header( str('ONE_TRACK'), '', $meta );
   else
-    page_header( str('MANY_TRACKS',$num_rows), '');
+    page_header( str('MANY_TRACKS',$num_rows), '', $meta );
 
-  // Display Information about current selection  
-  search_distinct_info($info, str('TRACK_NAME') ,'title' ,$sql_table, $predicate);
+  // Display Information about current selection
+  $track_name  = search_distinct_info($info, str('TRACK_NAME') ,'title' ,$sql_table, $predicate);
   $album_name  = search_distinct_info($info, str('ALBUM')      ,'album' ,$sql_table, $predicate);
   $artist_name = search_distinct_info($info, str('ARTIST')     ,'artist',$sql_table, $predicate);
   search_distinct_info($info, str('GENRE')      ,'genre' ,$sql_table, $predicate);
   search_distinct_info($info, str('YEAR')       ,'year'  ,$sql_table, $predicate);
   $info->add_item( str('MUSIC_PLAY_TIME'),  hhmmss($playtime));
-  
+
   // Build menu of options
-  $menu->add_item(str('PLAY_NOW'),   play_sql_list(MEDIA_TYPE_MUSIC,"select * from $sql_table $predicate order by album,lpad(disc,10,'0'),lpad(track,10,'0'),title") );
+  $menu->add_item(str('PLAY_NOW'), play_sql_list(MEDIA_TYPE_MUSIC,"select * from $sql_table $predicate order by album,lpad(disc,10,'0'),lpad(track,10,'0'),title") );
 
   // If MusicIP support is enabled then add an extra option
   if ( musicip_available() )
-    $menu->add_item( str('MIP_MIX_SONG'),musicip_mix_link( $sql_table, $predicate) );
-  
+    $menu->add_item( str('MIP_MIX_SONG'), musicip_mix_link( $sql_table, $predicate) );
+
   // Or adds these tracks to their current playlist...
   $menu->add_item(str('ADD_PLAYLIST'),'add_playlist.php?sql='.rawurlencode("select * from $sql_table $predicate order by album,lpad(disc,10,'0'),lpad(track,10,'0'),title"),true);
 
   // If only one track is selected, the user might want to expand their selection to the whole album
-  $album_name = db_value("select album from $sql_table $predicate");
   if ($num_rows ==1 && !empty($album_name))
     $menu->add_item( str('SELECT_ENTIRE_ALBUM'),'music_select_album.php?name='.rawurlencode($album_name));
 
@@ -68,9 +68,10 @@
     elseif ( !empty($album_name) )
       $menu->add_item( str("SEARCH_WIKIPEDIA"), lang_wikipedia_search( strip_title($album_name)), true);
   }
-  
-  // Is there a picture for us to display?
-  $folder_img = file_albumart( db_value("select concat(dirname,filename) from $sql_table $predicate limit 0,1") );
+
+  // Is there a picture for us to display? (only if selected media is in a single folder)
+  if ( db_value("select count(distinct dirname) from $sql_table $predicate")==1 )
+    $folder_img = file_albumart( db_value("select concat(dirname,filename) from $sql_table $predicate limit 0,1") );
 
   //*************************************************************************************************
   // Display the page
@@ -98,10 +99,15 @@
   }
 
   // Display ABC buttons
+  $buttons = array();
   if (!isset($_SESSION["shuffle"]) || $_SESSION["shuffle"] == 'off')
     $buttons[] = array('text'=>str('SHUFFLE_ON'), 'url'=> url_set_param($this_url,'shuffle','on') );
   else
     $buttons[] = array('text'=>str('SHUFFLE_OFF'), 'url'=> url_set_param($this_url,'shuffle','off') );
+  if (internet_available() && !empty($artist_name))
+    $buttons[] = array('text'=>str('ARTIST_INFO'), 'url'=> url_add_params('music_info.php', array('artist'=>urlencode($artist_name),
+                                                                                                  'album'=>urlencode($album_name),
+                                                                                                  'track'=>urlencode($track_name))) );
 
   page_footer( url_add_params( search_picker_most_recent(), array("p_del"=>"y","del"=>"y") ), $buttons );
 
