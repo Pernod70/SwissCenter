7c9d24c293fed9e2b1696bc9b07c97e0e1fd2dfa
Nigel Barnes <1696956+Pernod70@users.noreply.github.com>
2009-11-09 22:59:26 +0000

    Fixed patch_0026 which would fail with: Column 'media_type' in field list is ambiguous

--- swisscenter/database/patch_0026.sql
+++ swisscenter/database/patch_0026.sql
@@ -2,7 +2,8 @@
 -- Remove DVD Video media type and re-allocate existing media to Video media type
 -- -------------------------------------------------------------------------------------------------
 
-UPDATE media_locations, viewings SET media_type=3 WHERE media_type=7;
+UPDATE media_locations SET media_type=3 WHERE media_type=7;
+UPDATE viewings        SET media_type=3 WHERE media_type=7;
 DELETE FROM media_types WHERE media_id=7;
 
 -- -------------------------------------------------------------------------------------------------
