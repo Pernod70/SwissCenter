155a213ee2e3c271e7468b1f13e02bd6631a2192
Nigel Barnes <1696956+Pernod70@users.noreply.github.com>
2009-12-31 13:50:51 +0000

    Added a system preference PHOTO_DELAY_SECONDS (default 0,5,10,15,30,60,90) which can be set to determine the preferred photo delay options. Fixes #199
    The photo change music, change order, and change time pages have been updated to use the photo style.

--- swisscenter/photo_change_music.php
+++ swisscenter/photo_change_music.php
@@ -6,8 +6,8 @@
   require_once( realpath(dirname(__FILE__).'/base/menu.php'));
   require_once( realpath(dirname(__FILE__).'/base/mysql.php'));
   require_once( realpath(dirname(__FILE__).'/base/language.php'));
-  require_once( realpath(dirname(__FILE__).'/base/search.php'));  
-  
+  require_once( realpath(dirname(__FILE__).'/base/search.php'));
+
   $back_url = search_hist_most_recent();
 
   if (isset($_REQUEST["music"]))
@@ -15,9 +15,9 @@
   	$_SESSION['background_music'] = urldecode($_REQUEST["music"]);
   	header('Location: '.$back_url["url"]);
   }
-  else 
+  else
   {
-    page_header( str('PHOTOS_MUSIC_CHANGE'), '');
+    page_header( str('VIEW_PHOTO'),str('PHOTOS_MUSIC_CHANGE'),'',1,false,'',MEDIA_TYPE_PHOTO );
 
     echo '<p>';
     $menu = new menu();
@@ -25,10 +25,10 @@
     $menu->add_item( str('PHOTOS_MUSIC_NONE'),        'photo_change_music.php?music=');
     $menu->add_item( str('PHOTOS_MUSIC_CURRENT'),     'photo_change_music.php?music='.urlencode('*'));
 
-    $menu->display();
+    $menu->display(1, style_value("MENU_PHOTO_WIDTH"), style_value("MENU_PHOTO_ALIGN"));
     page_footer( $back_url["url"] );
   }
-  
+
 /**************************************************************************************************
                                                End of file
  **************************************************************************************************/
--- swisscenter/photo_change_order.php
+++ swisscenter/photo_change_order.php
@@ -6,24 +6,24 @@
   require_once( realpath(dirname(__FILE__).'/base/menu.php'));
   require_once( realpath(dirname(__FILE__).'/base/mysql.php'));
   require_once( realpath(dirname(__FILE__).'/base/language.php'));
-  require_once( realpath(dirname(__FILE__).'/base/search.php'));  
-  
+  require_once( realpath(dirname(__FILE__).'/base/search.php'));
+
   $back_url = search_hist_most_recent();
 
   if (isset($_REQUEST["order"]))
   {
-    // User has selected "Random"? - Sorting by the "dirname" column is a botch (we have to sort by *something*). 
+    // User has selected "Random"? - Sorting by the "dirname" column is a botch (we have to sort by *something*).
     if ($_REQUEST["order"] == 'dirname')
       $_SESSION["shuffle"] = 'on';
-    else 
+    else
       $_SESSION["shuffle"] = 'off';
-    
+
   	set_user_pref('PHOTO_PLAY_ORDER',$_REQUEST["order"]);
   	header('Location: '.$back_url["url"]);
   }
-  else 
+  else
   {
-    page_header( str('PHOTO_CHANGE_ORDER'), '');
+    page_header( str('VIEW_PHOTO'),str('PHOTO_CHANGE_ORDER'),'',1,false,'',MEDIA_TYPE_PHOTO );
 
     echo '<p>';
     $menu = new menu();
@@ -33,10 +33,10 @@
     $menu->add_item( str('PHOTO_ORDER_DATE_DISK'),   'photo_change_order.php?order=date_modified');
     $menu->add_item( str('PHOTO_ORDER_DATE_RANDOM'), 'photo_change_order.php?order=dirname');
 
-    $menu->display();
+    $menu->display(1, style_value("MENU_PHOTO_WIDTH"), style_value("MENU_PHOTO_ALIGN"));
     page_footer( $back_url["url"] );
   }
-  
+
 /**************************************************************************************************
                                                End of file
  **************************************************************************************************/
