de5cd74292582cf7848bcbc7e216c8ce3065ec5e
Pernod <Pernod@5fb36b96-46d7-3642-b0fa-166f5347550f>
2009-12-31 13:50:51 +0000

    Added a system preference PHOTO_DELAY_SECONDS (default 0,5,10,15,30,60,90) which can be set to determine the preferred photo delay options. Fixes #199
    The photo change music, change order, and change time pages have been updated to use the photo style.

--- trunk/swisscenter/photo_change_time.php
+++ trunk/swisscenter/photo_change_time.php
@@ -6,8 +6,8 @@
   require_once( realpath(dirname(__FILE__).'/base/menu.php'));
   require_once( realpath(dirname(__FILE__).'/base/mysql.php'));
   require_once( realpath(dirname(__FILE__).'/base/language.php'));
-  require_once( realpath(dirname(__FILE__).'/base/search.php'));  
-  
+  require_once( realpath(dirname(__FILE__).'/base/search.php'));
+
   $back_url = search_hist_most_recent();
 
   if (isset($_REQUEST["time"]))
@@ -15,24 +15,21 @@
   	set_user_pref('PHOTO_PLAY_TIME',$_REQUEST["time"]);
   	header('Location: '.$back_url["url"]);
   }
-  else 
+  else
   {
-    page_header( str('PHOTO_CHANGE_TIME'), '');
+    page_header( str('VIEW_PHOTO'),str('PHOTO_CHANGE_TIME'),'',1,false,'',MEDIA_TYPE_PHOTO );
 
     echo '<p>';
     $menu = new menu();
 
-    $menu->add_item( str('SECONDS',5),        'photo_change_time.php?time=5');
-    $menu->add_item( str('SECONDS',10),        'photo_change_time.php?time=10');
-    $menu->add_item( str('SECONDS',15),        'photo_change_time.php?time=15');
-    $menu->add_item( str('SECONDS',30),        'photo_change_time.php?time=30');
-    $menu->add_item( str('SECONDS',60),        'photo_change_time.php?time=60');
-    $menu->add_item( str('SECONDS',90),        'photo_change_time.php?time=90');
+    $time = get_sys_pref('PHOTO_DELAY_SECONDS', '0,5,10,15,30,60,90');
+    foreach (explode(',', $time) as $delay)
+      $menu->add_item( str('SECONDS',trim($delay)), 'photo_change_time.php?time='.trim($delay));
 
-    $menu->display();
+    $menu->display(1, style_value("MENU_PHOTO_WIDTH"), style_value("MENU_PHOTO_ALIGN"));
     page_footer( $back_url["url"] );
   }
-  
+
 /**************************************************************************************************
                                                End of file
  **************************************************************************************************/
