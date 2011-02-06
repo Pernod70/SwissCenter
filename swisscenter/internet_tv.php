c9860c00bd7e52fbc6b12b160477d1d125f30552
Nigel Barnes <1696956+Pernod70@users.noreply.github.com>
2011-02-06 19:25:00 +0000

    Enabled new Internet TV options: - Film Trailers. Fixes #222 - FTV Girls (restricted to users with adult rating). Fixes #235

--- swisscenter/internet_tv.php
+++ swisscenter/internet_tv.php
@@ -4,17 +4,28 @@
  *************************************************************************************************/
 
   require_once( realpath(dirname(__FILE__).'/base/page.php'));
+  require_once( realpath(dirname(__FILE__).'/base/users.php'));
 
   // Display the page
   page_header(str('WATCH_INTERNET_TV'), '','',1,false,'',MEDIA_TYPE_INTERNET_TV);
 
   $menu = new menu();
+//  $menu->add_item( str('SHOUTCAST_TV'), 'shoutcast_tv.php' );
+
+  // Options only available on PCH
   if ( is_pc() || get_player_model() > 400 )
   {
     $menu->add_item( str('YOUTUBE'), 'youtube_menu.php' );
     $menu->add_item( str('APPLE_TRAILERS'), 'apple_trailer.php' );
   }
+  $menu->add_item( str('FILM_TRAILERS'), 'film_trailer.php' );
   $menu->add_item( str('TOMA_INTERNET_TV'), 'internet_tv_toma.php' );
+
+  // Adult restricted options
+  if (get_current_user_rank() >= 100)
+  {
+    $menu->add_item( str('FTV_GIRLS'), 'ftvgirls.php' );
+  }
   $menu->add_item( str('BOOKMARKS'), 'internet_tv_urls.php' );
 
   $menu->display(1, style_value("MENU_INTERNET_TV_WIDTH"), style_value("MENU_INTERNET_TV_ALIGN"));
