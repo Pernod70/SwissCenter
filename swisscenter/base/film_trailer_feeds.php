78fe8e25c1e6b49652f5dbc3dd07c92440caacf8
Nigel Barnes <1696956+Pernod70@users.noreply.github.com>
2011-02-11 01:28:04 +0000

    Changed the file format of trailers from FilmTrailer.com. The wmv files did not play well on many players so we now get the mp4 files.
    Refs #222

--- swisscenter/base/film_trailer_feeds.php
+++ swisscenter/base/film_trailer_feeds.php
@@ -7,7 +7,7 @@ require_once( realpath(dirname(__FILE__).'/mysql.php'));
 
 define('FILMTRAILER_URL','http://www.services.filmtrailer.com');
 define('FILMTRAILER_CHANNEL_USER_ID', '41100914-1'); // Unique ID for SwissCenter
-define('FILMTRAILER_FILE_TYPE', 'wmv');              // flv, mp4, mov, wmv
+define('FILMTRAILER_FILE_TYPE', 'mp4');              // flv, mp4, mov, wmv
 define('FILMTRAILER_FILE_SIZE', 'xxlarge');          // small, medium, large, xlarge, xxlarge
 
 class FilmTrailer {
