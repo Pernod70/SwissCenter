731b3f490f15efd5409b3691b9ab7353396f244d
Nigel Barnes <1696956+Pernod70@users.noreply.github.com>
2011-03-13 18:52:43 +0000

    Added the font width multiplier settings for the various player types to the Miscellaneous configuration page.
    Refs #10

--- swisscenter/config/config_misc.php
+++ swisscenter/config/config_misc.php
@@ -10,7 +10,11 @@
 
   function misc_display( $message = '')
   {
-  	$fontwidth_multiplier = (!empty($_REQUEST["fontwidth_multiplier"]) ? $_REQUEST["fontwidth_multiplier"] : get_sys_pref("FONTWIDTH_MULTIPLIER",1.0));
+    $fontwidth_multiplier = array();
+    $fontwidth_multiplier['100'] = (!empty($_REQUEST["fontwidth_multiplier_100"]) ? $_REQUEST["fontwidth_multiplier_100"] : get_sys_pref('FONTWIDTH_MULTIPLIER_100',1.2));
+    $fontwidth_multiplier['200'] = (!empty($_REQUEST["fontwidth_multiplier_200"]) ? $_REQUEST["fontwidth_multiplier_200"] : get_sys_pref('FONTWIDTH_MULTIPLIER_200',1.2));
+    $fontwidth_multiplier['400'] = (!empty($_REQUEST["fontwidth_multiplier_400"]) ? $_REQUEST["fontwidth_multiplier_400"] : get_sys_pref('FONTWIDTH_MULTIPLIER_400',1.35));
+    $fontwidth_multiplier['pc']  = (!empty($_REQUEST["fontwidth_multiplier_pc"]) ? $_REQUEST["fontwidth_multiplier_pc"] : get_sys_pref('FONTWIDTH_MULTIPLIER_PC',1.0));
     $pc_screen_size_opts  = array( "800x450"           => "800x450",
                                    "1024x768"          => "1024x768",
                                    "1280x1024"         => "1280x1024",
@@ -26,10 +30,17 @@
     form_hidden('action','UPDATE');
     form_input('fontname',str('TTF_FONT'),30,'',get_sys_pref('TTF_FONT'));
     form_label(str('TTF_FONT_PROMPT'));
-    form_input('fontwidth_multiplier',str('FONTWIDTH_MULTIPLIER'),3,'', $fontwidth_multiplier);
+    echo '<tr><td>'.str('FONTWIDTH_MULTIPLIER').' : &nbsp;<td><tr>';
+    form_input('fontwidth_multiplier_100',str('FONTWIDTH_MULTIPLIER_100'),3,'', $fontwidth_multiplier['100']);
+    form_input('fontwidth_multiplier_200',str('FONTWIDTH_MULTIPLIER_200'),3,'', $fontwidth_multiplier['200']);
+    form_input('fontwidth_multiplier_400',str('FONTWIDTH_MULTIPLIER_400'),3,'', $fontwidth_multiplier['400']);
+    form_input('fontwidth_multiplier_pc',str('FONTWIDTH_MULTIPLIER_PC'),3,'', $fontwidth_multiplier['pc']);
     form_label(str('FONTWIDTH_MULTIPLIER_PROMPT'));
     form_list_static('pc_screen_size',str('PC_SCREEN_SIZE'), $pc_screen_size_opts, get_sys_pref('PC_SCREEN_SIZE','800x450'), false, false, false);
     form_label(str('PC_SCREEN_SIZE_PROMPT'));
+//    form_input('player_page_charset',str('PLAYER_PAGE_CHARSET'),15,'', get_sys_pref('PLAYER_PAGE_CHARSET','Windows-1252'));
+//    form_input('config_page_charset',str('CONFIG_PAGE_CHARSET'),15,'', get_sys_pref('CONFIG_PAGE_CHARSET','Windows-1252'));
+//    form_label(str('CHARSET_PROMPT'));
     form_input('date_format',str('DATE_FORMAT'),15,'',get_sys_pref('DATE_FORMAT','%d%b%y'));
     form_label(str('DATE_FORMAT_TEST', db_value("select date_format(now(),'".get_sys_pref('DATE_FORMAT','%d%b%y')."')")));
     form_label(str('DATE_FORMAT_PROMPT'));
@@ -45,16 +56,27 @@
   {
     $img = new CImage();
     $msg = '';
+    $fontwidth_msg = '';
 
-    $fontwidth_multiplier = $_REQUEST["fontwidth_multiplier"];
+    $fontwidth_multiplier = array();
     $pc_screen_size       = $_REQUEST["pc_screen_size"];
-    $fontname             = $_REQUEST["fontname"];
+//    $player_page_charset  = $_REQUEST["player_page_charset"];
+//    $config_page_charset  = $_REQUEST["config_page_charset"];
+    $fontname             = os_path($_REQUEST["fontname"]);
     $date_format          = $_REQUEST["date_format"];
 
-    if (! form_mask($fontwidth_multiplier,'[0-9]*'))
-      misc_display("!".str('MISC_FONTWIDTH_MULTIPLIER_NOT_NUMBER'));
-    elseif ( $fontwidth_multiplier < 0.5 )
-      misc_display("!".str('MISC_FONTWIDTH_MULTIPLIER_TOO_SMALL'));
+    $players = array('100', '200', '400', 'pc');
+    foreach ( $players as $player )
+    {
+      $fontwidth_multiplier[$player] = $_REQUEST["fontwidth_multiplier_$player"];
+      if (! form_mask($fontwidth_multiplier[$player],'[0-9]*'))
+        $fontwidth_msg = str('MISC_FONTWIDTH_NOT_NUMBER')." : ".$fontwidth_multiplier[$player];
+      elseif ( $fontwidth_multiplier[$player] < 0.5 )
+        $fontwidth_msg = str('MISC_FONTWIDTH_TOO_SMALL')." : ".$fontwidth_multiplier[$player];
+    }
+
+    if (!empty($fontwidth_msg))
+      misc_display("!".$fontwidth_msg);
     else
     {
       // Check to see if the font specified can be successfully used.
@@ -63,8 +85,12 @@
       else
         set_sys_pref('TTF_FONT',$fontname);
 
-      set_sys_pref('FONTWIDTH_MULTIPLIER',$fontwidth_multiplier);
+      foreach ( $players as $player )
+        set_sys_pref('FONTWIDTH_MULTIPLIER_'.$player,$fontwidth_multiplier[$player]);
+
       set_sys_pref('PC_SCREEN_SIZE',$pc_screen_size);
+//      set_sys_pref('PLAYER_PAGE_CHARSET',$player_page_charset);
+//      set_sys_pref('CONFIG_PAGE_CHARSET',$config_page_charset);
       set_sys_pref('DATE_FORMAT',$date_format);
       unset($_SESSION["device"]);
       misc_display(str('SAVE_SETTINGS_OK').$msg);
