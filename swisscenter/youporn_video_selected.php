ba380fcede8936d8ca93fafef05f81b1cc3ccc4f
Nigel Barnes <1696956+Pernod70@users.noreply.github.com>
2011-05-13 17:43:50 +0000

    Added ability to browse/search and play videos at YouPorn. This feature is only available to users assigned a Max Certificate Viewable of over 18.
    Fixed #260

---
+++ swisscenter/youporn_video_selected.php
@@ -0,0 +1,65 @@
+<?php
+/**************************************************************************************************
+   SWISScenter Source                                                              Nigel Barnes
+ *************************************************************************************************/
+
+require_once( realpath(dirname(__FILE__).'/base/page.php'));
+require_once( realpath(dirname(__FILE__).'/base/youporn.php'));
+
+function star_rating( $rating )
+{
+  // Form star rating
+  $img_rating = '';
+  if ( !is_null($rating) )
+  {
+    $user_rating = nvl($rating*2,0);
+    for ($i = 1; $i<=10; $i++)
+    {
+      if ( $user_rating >= $i )
+        $img_rating .= img_gen(style_img('STAR',true),25,40);
+      elseif ( $i-1 >= $user_rating )
+        $img_rating .= img_gen(style_img('STAR_0',true),25,40);
+      else
+        $img_rating .= img_gen(style_img('STAR_'.(number_format($user_rating,1)-floor($user_rating))*10,true),25,40);
+    }
+  }
+  return $img_rating;
+}
+
+/**************************************************************************************************
+   Main page output
+ *************************************************************************************************/
+
+  $url   = $_REQUEST["url"];
+  $image = $_REQUEST["img"];
+
+  // Get information about a video.
+  $youporn = new YouPorn();
+  $details = $youporn->getDetails($url);
+
+  // Page headings
+  page_header($details["title"], star_rating($details["rating"]));
+
+  $menu = new menu();
+  $menu->add_item( str('PLAY_NOW'), 'href="'.url_add_params('stream_url.php?'.current_session(), array('url' => $details["video_url"],
+                                                                                                       'ext' => '.mpg" vod ')));
+  echo '<table width="100%" cellpadding=0 cellspacing=0 border=0>
+          <tr>
+            <td valign=top width="'.convert_x(280).'" align="left">'.img_gen($image,280,550).'</td>
+            <td width="'.convert_x(20).'"></td>
+            <td valign="top">'.
+              font_tags(FONTSIZE_BODY).str('RUNNING_TIME').': '.$details["duration"].'</font>
+              <p>'.font_tags(FONTSIZE_BODY).str('DATE').': '.$details["date"].'</font>
+              <p>'.font_tags(FONTSIZE_BODY).str('VIEWED').': '.$details["viewed"].'</font>';
+              $menu->display(1, 480);
+  echo     '</td>
+          </tr>
+        </table>';
+
+  // Make sure the "back" button goes to the correct page:
+  page_footer(page_hist_back_url());
+
+/**************************************************************************************************
+                                               End of file
+ **************************************************************************************************/
+?>
