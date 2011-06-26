a6f0548543e22de8e9af1afa70ecedd6f0f94dd0
Nigel Barnes <1696956+Pernod70@users.noreply.github.com>
2011-06-26 00:51:19 +0000

    The export details to XML sidecar files now uses the xmlbuilder class rather than XPath.
    Fixed #261

--- swisscenter/media_export_xml.php
+++ swisscenter/media_export_xml.php
@@ -10,45 +10,45 @@
   //===========================================================================================
   // Main script logic
   //===========================================================================================
-  
+
   $export_xml = get_sys_pref('EXPORT_XML');
   delete_sys_pref('EXPORT_XML');
-  
+
   switch ($export_xml)
   {
     case 'VIDEO':
       if ( get_sys_pref('movie_xml_save','NO') == 'YES' )
       {
         send_to_log(4,'Exporting all video information to XML');
-  
+
         // Only export videos information where the details_available column is 'Y'.
-        $data = db_toarray("select file_id from movies where details_available = 'Y' ");
-      
+        $data = db_toarray("select file_id from movies where details_available = 'Y' order by dirname,title ");
+
         // Process each video file
         foreach ($data as $row)
           export_video_to_xml( $row["FILE_ID"] );
-            
+
         send_to_log(4,'Export of all video information to XML complete');
       }
       break;
-      
+
     case 'TV':
       if ( get_sys_pref('tv_xml_save','NO') == 'YES' )
       {
         send_to_log(4,'Exporting all tv information to XML');
-  
+
         // Only export tv information where the details_available column is 'Y'.
-        $data = db_toarray("select file_id from tv where details_available = 'Y' ");
-      
+        $data = db_toarray("select file_id from tv where details_available = 'Y' order by dirname,programme ");
+
         // Process each tv file
         foreach ($data as $row)
           export_tv_to_xml( $row["FILE_ID"] );
-            
+
         send_to_log(4,'Export of all tv information to XML complete');
       }
       break;
   }
-  
+
 /**************************************************************************************************
                                                End of file
  **************************************************************************************************/
