83e0a774b2f0cde48dedfe1ad896d68bed282c5e
Pernod <Pernod@5fb36b96-46d7-3642-b0fa-166f5347550f>
2011-11-19 15:38:24 +0000

    Added option to view lyrics from the music selected page. Lyrics are retrieved from the MetroLyrics site and the option is only available if a single track is selected.
    Closed #270

---
+++ trunk/swisscenter/music_lyrics.php
@@ -0,0 +1,47 @@
+<?php
+/**************************************************************************************************
+   SWISScenter Source                                                              Nigel Barnes
+ *************************************************************************************************/
+
+  require_once( realpath(dirname(__FILE__).'/base/page.php'));
+  require_once( realpath(dirname(__FILE__).'/resources/lyrics/metrolyrics.php'));
+
+/**************************************************************************************************
+   Main page output
+ *************************************************************************************************/
+
+  // Get artist and track details
+  $artist = $_REQUEST["artist"];
+  $track  = $_REQUEST["track"];
+
+  // Search for lyrics
+  $metro  = new MetroLyrics();
+  $lyrics = $metro->getLyrics($artist, $track);
+
+  // Check for no lyrics available
+  if ( strpos($lyrics, 'www.metrolyrics.com') > 0 )
+  {
+    page_inform(2, page_hist_previous(), $artist.' - '.$track,str('NO_LYRICS_AVAILABLE'));
+  }
+  else
+  {
+    // Display the lyrics
+    page_header( $artist.' - '.$track );
+
+    if (is_pc())
+      echo '<div style="height:'.convert_y(750).'; overflow:scroll;">';
+
+    echo '<table '.style_background('PAGE_TEXT_BACKGROUND').' width="100%" cellpadding="5" cellspacing="0" border="0" align="center">';
+    echo '<tr><td><center>'.font_tags(FONTSIZE_BODY).$lyrics.'</font><center></td></tr>';
+    echo '</table>';
+
+    if (is_pc())
+      echo '</div>';
+
+    page_footer(page_hist_previous());
+  }
+
+/**************************************************************************************************
+                                               End of file
+ **************************************************************************************************/
+?>
