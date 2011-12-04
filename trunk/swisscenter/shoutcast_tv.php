1ad90259ebf54a7fda168ad2e576628f3f540381
Pernod <Pernod@5fb36b96-46d7-3642-b0fa-166f5347550f>
2011-12-04 18:41:27 +0000

    Completed the menu system for SHOUTcastTV.
    Refs #201

--- trunk/swisscenter/shoutcast_tv.php
+++ trunk/swisscenter/shoutcast_tv.php
@@ -10,16 +10,15 @@
 /**
  * Displays a menu of genres and allows you to choose one.
  *
- * @param array $directory
+ * @param array $genres
  */
 
 function display_shoutcast_tv_genres($genres)
 {
   $current_url = url_remove_param(current_url(), 'page');
-  $select_url = url_remove_param($current_url, 'filter');
 
   foreach ($genres as $name=>$count)
-    $array[] = array("name"=>$name.' ('.$count.')', "url"=>url_set_param( $select_url, 'genre', rawurlencode($name) ));
+    $array[] = array("name"=>$name.' ('.$count.')', "url"=>url_add_param('shoutcast_tv_search.php', 'genre', $name) );
 
   $page = (isset($_REQUEST["page"]) ? $_REQUEST["page"] : 0);
 
@@ -34,22 +33,7 @@ $current_url = url_remove_param(current_url(), 'filter');
 
 $shoutcast = new SHOUTcastTV();
 
-if ( isset($_REQUEST["menu"]) )
-{
-  // Set subtitle for current menu
-  switch ($_REQUEST["menu"])
-  {
-    case 'genres':            { $subtitle = str('BROWSE_GENRE'); break; }
-    case 'studios':           { $subtitle = str('BROWSE_STUDIO'); break; }
-  }
-}
-else
-{
-  $subtitle = '';
-}
-
-page_header( str('SHOUTCAST_TV'), $subtitle );
-$buttons = array();
+page_header( str('SHOUTCAST_TV') );
 
 echo '<table width="100%" cellpadding=0 cellspacing=0 border=0>
         <tr>
@@ -59,19 +43,11 @@ echo '<table width="100%" cellpadding=0 cellspacing=0 border=0>
           <td width="'.convert_x(20).'"></td>
           <td valign="top">';
 
-if ( isset($_REQUEST["menu"]) && $_REQUEST["menu"] == 'genres' )
-{
-  $back_url = apple_trailer_page_params();
-  display_apple_trailer_menu( get_apple_trailers_genres() );
-}
-
-
 if (isset($_REQUEST["filter"]) && $_REQUEST["filter"] == 'genre')
 {
   // Select genre
   $genres = $shoutcast->getGenres();
   display_shoutcast_tv_genres($genres);
-  $back_url = $current_url;
 }
 else
 {
@@ -79,12 +55,11 @@ else
   $menu = new menu();
   $menu->add_item(str('BROWSE_TITLE'), url_add_param('shoutcast_tv_search.php', 'sort', 'name'));
   $menu->add_item(str('BROWSE_RATING'), url_add_param('shoutcast_tv_search.php', 'sort', 'viewers'));
-  $menu->add_item(str('BROWSE_GENRE'), url_add_param($current_url,'filter','genre') );
+  $menu->add_item(str('BROWSE_GENRE'), url_add_param($current_url, 'filter', 'genre') );
   $menu->display(1, 520);
-  $back_url = 'internet_tv.php';
 }
 
-page_footer( $back_url );
+page_footer( page_hist_previous() );
 
 /**************************************************************************************************
                                                End of file
