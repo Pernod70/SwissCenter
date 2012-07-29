8a8cb71dd04becd3d54fb8cc4eccd7fcc1aa86d8
Nigel Barnes <1696956+Pernod70@users.noreply.github.com>
2012-07-29 01:10:10 +0000

    Fixed HTML entity encoding for href links on the AZ picker.

--- swisscenter/base/az_picker.php
+++ swisscenter/base/az_picker.php
@@ -19,17 +19,17 @@ function show_picker( $url="", $search, $case = '', $validchars = '' )
     echo '<table cellspacing=2 border=0 '.style_background('KEYBOARD_BACKGROUND').'><tr>';
     $url = url_set_params($url, array('last'=>'KEY_SPC', 'search'=>rawurlencode($search.' ')));
     echo '<td colspan=3 height="'.convert_y(50).'" '.style_background('KEYBOARD_KEY_BACKGROUND').'
-           align="center"><a href="'.$url.'" style="width:'.convert_x(105).'"name="KEY_SPC">'.font_tags(FONTSIZE_KEYPAD).str('KEYBOARD_SPC').'</a></td>';
+           align="center"><a href="'.htmlspecialchars($url).'" style="width:'.convert_x(105).'" name="KEY_SPC">'.font_tags(FONTSIZE_KEYPAD).str('KEYBOARD_SPC').'</font></a></td>';
     $url = url_set_params($url, array('last'=>'KEY_DEL', 'search'=>rawurlencode(substr($search,0,-1))));
     echo '<td colspan=3 height="'.convert_y(50).'" '.style_background('KEYBOARD_KEY_BACKGROUND').'
-           align="center"><a href="'.$url.'" style="width:'.convert_x(105).'"name="KEY_DEL">'.font_tags(FONTSIZE_KEYPAD).str('KEYBOARD_DEL').'</a></td>';
+           align="center"><a href="'.htmlspecialchars($url).'" style="width:'.convert_x(105).'" name="KEY_DEL">'.font_tags(FONTSIZE_KEYPAD).str('KEYBOARD_DEL').'</font></a></td>';
     echo '</tr><tr>';
 
     for ($n=1; $n<=strlen($keys); $n++)
     {
       $this_key = substr($keys,($n-1),1);
       $url = url_set_params($url, array('last'=>'KEY_'.$this_key, 'search'=>rawurlencode($search.$this_key)));
-      $link = '<a href="'.$url.'" name="KEY_'.$this_key.'">'.font_tags(FONTSIZE_KEYPAD).$this_key.'</a>';
+      $link = '<a href="'.htmlspecialchars($url).'" name="KEY_'.$this_key.'">'.font_tags(FONTSIZE_KEYPAD).$this_key.'</font></a>';
 
       echo '<td width="'.convert_x(35).'" height="'.convert_y(50).'"'.style_background('KEYBOARD_KEY_BACKGROUND').'
                 align="center">'.$link.'</td>';
