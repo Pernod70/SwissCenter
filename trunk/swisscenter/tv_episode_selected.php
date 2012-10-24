5a00c65bfd1225200ed2e3aa27f8178b47057daa
Pernod <Pernod@5fb36b96-46d7-3642-b0fa-166f5347550f>
2012-10-24 23:10:50 +0000

    Fixed potential PHP warnings being generated when no themes are chosen for movies and TV series.

--- trunk/swisscenter/tv_episode_selected.php
+++ trunk/swisscenter/tv_episode_selected.php
@@ -135,7 +135,7 @@
 
   // Random fanart image
   $themes = db_toarray('select processed_image, show_banner, show_image from themes where media_type='.MEDIA_TYPE_TV.' and title="'.db_escape_str($data["PROGRAMME"]).'" and use_synopsis=1 and processed_image is not NULL');
-  $theme = $themes[mt_rand(0,count($themes)-1)];
+  $theme = (count($themes) > 0 ? $themes[mt_rand(0,count($themes)-1)] : '');
 
   // Set banner image
   if ( !empty($theme) && !$theme['SHOW_BANNER'] )
