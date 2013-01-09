b0463ac383cb855b624d8c3af05f99603f31fe66
Pernod <Pernod@5fb36b96-46d7-3642-b0fa-166f5347550f>
2013-01-09 16:17:17 +0000

    Ensure correct permissions are applied to folders created when downloading styles.

--- trunk/swisscenter/download_style.php
+++ trunk/swisscenter/download_style.php
@@ -6,7 +6,7 @@
   require_once( realpath(dirname(__FILE__).'/base/page.php'));
   require_once( realpath(dirname(__FILE__).'/base/prefs.php'));
   require_once( realpath(dirname(__FILE__).'/base/file.php'));
-  
+
   set_time_limit(60*5);
 
   $style    = rawurldecode($_REQUEST["name"]);
@@ -14,19 +14,21 @@
   $filename = SC_LOCATION.'_tmp_style.zip';
   $zipfile  = str_replace(' ','%20','http://update.swisscenter.co.uk/styles/'.$style.'.zip');
   send_to_log(5,'User has requested a download of the style "'.$style.'"');
-  
+
   // First attempt to get the style
   if ( ($zip = file_get_contents($zipfile)) !== false)
   {
     // Write the zipfile to a temporary file
     write_binary_file($filename,$zip);
-    
+
     // Delete the directory if it already exists (an update?)
     if ( file_exists($dir))
       force_rmdir($dir);
 
     // Create the directory
-    mkdir($dir);
+    $oldumask = umask(0);
+    mkdir($dir,0777);
+    umask($oldumask);
 
     // Extract the zipfile
     if ( file_exists($dir))
@@ -39,7 +41,7 @@
           $ze_filename = zip_entry_name($zip_entry);
           if (zip_entry_open($fh, $zip_entry, "r"))
           {
-            $buf = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));  
+            $buf = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
             zip_entry_close($zip_entry);
             write_binary_file($dir.$ze_filename,$buf);
             send_to_log(8,'Extracting file from zipfile : '.$dir.$ze_filename);
@@ -49,18 +51,18 @@
       }
       elseif (is_unix())
       {
-        // On LINUX machines, we can write to a temporary file and then use the standard "unzip" command to 
+        // On LINUX machines, we can write to a temporary file and then use the standard "unzip" command to
         // perform the same functions as the zip extension
         exec('unzip '.$filename.' -d '.$dir);
       }
     }
-    
+
     // Delete temporary file
     unlink($filename);
   }
-  else 
+  else
     send_to_log(1,'Unable to download the style',array("Style Name"=>$style,"Zipfile"=>$zipfile));
-  
+
   // Set the style in the swssion, and redirect back to the main style page.
   set_user_pref('style',$style);
   load_style();
