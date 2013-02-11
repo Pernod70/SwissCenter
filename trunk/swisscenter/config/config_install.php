872c7e916186889f589f4f3f4df2d74e4fef7975
Pernod <Pernod@5fb36b96-46d7-3642-b0fa-166f5347550f>
2013-02-11 01:57:13 +0000

    Updated the Create Database option in config to ensure utf8 charset and collation.

--- trunk/swisscenter/config/config_install.php
+++ trunk/swisscenter/config/config_install.php
@@ -58,16 +58,16 @@
     $db_stat = test_db(DB_HOST,'root',$pass,DB_DATABASE);
 
     if ($db_stat == 'OK')
-      db_root_sqlcommand($pass,"Drop database ".DB_DATABASE);
+      db_root_sqlcommand($pass,"DROP DATABASE ".DB_DATABASE);
 
-    if     ( db_root_sqlcommand($pass,"Create database ".DB_DATABASE) == false)
+    if     ( db_root_sqlcommand($pass,"CREATE DATABASE ".DB_DATABASE) == false)
       install_display('!'.str('DB_CREATE_DB_ERR'));
-    elseif ( db_root_sqlcommand($pass,"Grant all on ".DB_DATABASE.".* to ".DB_USERNAME."@'".DB_HOST."' identified by '".DB_PASSWORD."'") == false)
+    elseif ( db_root_sqlcommand($pass,"GRANT ALL ON ".DB_DATABASE.".* TO ".DB_USERNAME."@'".DB_HOST."' IDENTIFIED BY '".DB_PASSWORD."'") == false)
       install_display('!'.str('DB_CREATE_USER_ERR'));
     else
     {
       // Set character set and collation.
-      @db_root_sqlcommand($pass,"ALTER DATABASE ".DB_DATABASE." CHARACTER SET latin1 COLLATE latin1_swedish_ci");
+      @db_root_sqlcommand($pass,"ALTER DATABASE ".DB_DATABASE." CHARACTER SET utf8 COLLATE utf8_general_ci");
 
       // Set database password.
       @db_root_sqlcommand($pass,"SET PASSWORD FOR ".DB_USERNAME."@'".DB_HOST."' = PASSWORD('".DB_PASSWORD."')");
@@ -85,7 +85,7 @@
         // Default cache, playlists location and limit (default is no limit)
         set_sys_pref('CACHE_DIR',SC_LOCATION.'cache');
         set_sys_pref('PLAYLISTS',SC_LOCATION.'playlists');
-        set_sys_pref('CACHE_MAXSIZE_MB','20');
+        set_sys_pref('CACHE_MAXSIZE_MB','100');
 
         // Display the config page
         header('Location: index.php');
