f2d5dfdabc432c5ee2223e424ee0bb92c5c1ad3f
Pernod <Pernod@5fb36b96-46d7-3642-b0fa-166f5347550f>
2013-02-18 16:00:06 +0000

    Replaced tab formatting with spaces in config_phpinfo.php.

--- trunk/swisscenter/config/config_phpinfo.php
+++ trunk/swisscenter/config/config_phpinfo.php
@@ -16,17 +16,17 @@
     phpinfo(INFO_GENERAL | INFO_CONFIGURATION | INFO_MODULES | INFO_VARIABLES);
     $phpinfo = ob_get_clean();
 
-		$phpinfo = trim($phpinfo);
+    $phpinfo = trim($phpinfo);
 
-		// Here we play around a little with the PHP Info HTML to try and stylise
-		// it along SwissCenter's lines ... hopefully without breaking anything.
-		preg_match_all('#<body[^>]*>(.*)</body>#si', $phpinfo, $output);
-		$output = $output[1][0];
-		$output = preg_replace('#<a[^>]*><img[^>]*></a>#i', '', $output);
-		$output = preg_replace('#<table[^>]+>#i', '<table class="form_select_tab" width="600">', $output);
-		$output = str_replace(array('class="e"', 'class="v"', 'class="h"', '<hr />'), array('class="stdformlabel"', '', '', ''), $output);
+    // Here we play around a little with the PHP Info HTML to try and stylise
+    // it along SwissCenter's lines ... hopefully without breaking anything.
+    preg_match_all('#<body[^>]*>(.*)</body>#si', $phpinfo, $output);
+    $output = $output[1][0];
+    $output = preg_replace('#<a[^>]*><img[^>]*></a>#i', '', $output);
+    $output = preg_replace('#<table[^>]+>#i', '<table class="form_select_tab" width="600">', $output);
+    $output = str_replace(array('class="e"', 'class="v"', 'class="h"', '<hr />'), array('class="stdformlabel"', '', '', ''), $output);
 
-		// Fix invalid anchor names (eg "module_Zend Optimizer")
+    // Fix invalid anchor names (eg "module_Zend Optimizer")
     $output = str_replace('module_Zend Optimizer', 'module_Zend_Optimizer', $output);
 
     echo '<center>'.$output.'</center>'';
