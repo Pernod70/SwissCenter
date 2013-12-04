b49cbf9e5f0d72ea1b16c311880f7a8e84809ea9
Nigel Barnes <1696956+Pernod70@users.noreply.github.com>
2013-12-04 10:55:47 +0000

    Fixed references to ARTIST_SORT that should be SORT_ARTIST.

--- swisscenter/base/image_screens.php
+++ swisscenter/base/image_screens.php
@@ -330,7 +330,7 @@
         break;
       case 'DISCOGS':
       case 'LASTFM':
-        $fanart_img = get_discogs_artist_image( $current_track["ARTIST_SORT"] );
+        $fanart_img = get_discogs_artist_image( $current_track["SORT_ARTIST"] );
         break;
       default:
         $fanart_img = false;
