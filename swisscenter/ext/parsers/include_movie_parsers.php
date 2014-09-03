c62dafee136dbca496643fd67b4127cc1d1ae516
Nigel Barnes <1696956+Pernod70@users.noreply.github.com>
2014-09-03 01:02:11 +0000

    Added a check to ensure only valid parsers are loaded. Any invalid parser files are logged.

--- swisscenter/ext/parsers/include_movie_parsers.php
+++ swisscenter/ext/parsers/include_movie_parsers.php
@@ -8,16 +8,22 @@ require_once( realpath(dirname(__FILE__).'/ParserConstants.php'));
 require_once( realpath(dirname(__FILE__).'/ParserInterface.php'));
 require_once( realpath(dirname(__FILE__).'/Parser.php'));
 
+$type = 'movie';
+
 //Include the parser classes
-$path_to_classes = dir_to_array( realpath(dirname(__FILE__).'/movie/'), '.*\.php' );
+$path_to_classes = dir_to_array( realpath(dirname(__FILE__).'/'.$type.'/'), '.*\.php' );
 
 $files = array();
 foreach ($path_to_classes as $file)
 {
-  if ( is_parser($file) )
+  if ( is_parser($file, $type) )
   {
     $files[] = "$file";
     require_once( "$file" );
+  }
+  else
+  {
+    send_to_log(2, 'Parser file '.basename($file).' is not a valid '.$type.' parser and will be ignored');
   }
 }
 ?>
